5cf26a6a13b4481aaada4f735beb68ae1f730611
LJThao <yengfong.thao@atlasschool.com>
2024-09-03 00:15:09 -0500

    task 0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -8,4 +8,6 @@ def line():
     """Plotting a y line graph"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-
+    # plotting the data y with a red color line
+    plt.plot(y, color="red")
+    plt.xlim([0, 10])
