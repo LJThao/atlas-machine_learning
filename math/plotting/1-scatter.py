4dff8232793f4ae5917241441bafbc2fe9bf7bc5
LJThao <yengfong.thao@atlasschool.com>
2024-09-03 00:30:59 -0500

    task 1 and main files

---
+++ math/plotting/1-scatter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Plotting x -> y as a scatter plot"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def scatter():
+    """Plotting a scatter plot"""
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+    plt.figure(figsize=(6.4, 4.8))
+    # plotting labeled x and y axis, title, and magenta points
+    plt.scatter(x, y, c="Magenta")
+    plt.xlabel("Height (in)")
+    plt.ylabel("Weight (lbs)")
+    plt.title("Men's Height vs Weight")
