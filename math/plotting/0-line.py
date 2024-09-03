27312fef43976f21fac0de435b38f6151c0ef0f1
LJThao <yengfong.thao@atlasschool.com>
2024-09-03 00:04:33 -0500

    execute files and task 0

---
+++ math/plotting/0-line.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Plotting y as a line graph"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def line():
+    """Plotting a y line graph"""
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
