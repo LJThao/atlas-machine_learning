fa41889e671a321709d1c982307cc88c38076e13
LJThao <yengfong.thao@atlasschool.com>
2024-09-03 03:41:16 +0000

    task 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Function that performs matrix multiplication"""
+import numpy as np
+
+def np_matmul(mat1, mat2):
+    """Returns matrix multiplication using @ operator"""
+    return mat1 @ mat2
