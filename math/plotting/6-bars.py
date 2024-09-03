93d57eedba81d958013885a66d76d6b638bd5907
LJThao <yengfong.thao@atlasschool.com>
2024-09-03 12:06:21 -0500

    pycodestyle

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 def bars():
     """A stacked bar graph"""
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4,3))
+    fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
     # plotting a stacked bar graph
     columns = ["Farrah", "Fred", "Felicia"]
@@ -15,7 +15,8 @@ def bars():
     colors = ["red", "yellow", "#ff8000", "#ffe5b4"]
     bottom = np.zeros(fruit[0].shape)
     for i in range(len(fruit)):
-        plt.bar(range(len(fruit[0])), fruit[i], color=colors[i], bottom=bottom, width=0.5)
+        plt.bar(range(len(fruit[0])), fruit[i], color=colors[i],
+                bottom=bottom, width=0.5)
         bottom = bottom + fruit[i]
     plt.ylabel("Quantity of Fruit")
     plt.ylim([0, 80])
