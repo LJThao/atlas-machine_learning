a45ecaace931edb0773d386fd89da983689c1a67
LJThao <yengfong.thao@atlasschool.com>
2024-09-03 03:41:41 +0000

    pycodestyle

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -2,6 +2,7 @@
 """Function that performs matrix multiplication"""
 import numpy as np
 
+
 def np_matmul(mat1, mat2):
     """Returns matrix multiplication using @ operator"""
     return mat1 @ mat2
