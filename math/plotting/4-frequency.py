6c6224a5b0bb202c6bc51fa75a3ff88a81abcb98
LJThao <yengfong.thao@atlasschool.com>
2024-09-03 01:18:03 -0500

    added limits to x and y

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -13,5 +13,7 @@ def frequency():
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
     plt.title("Project A")
+    plt.xlim([0, 100])
+    plt.ylim([0, 30])
     plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
     plt.show()
