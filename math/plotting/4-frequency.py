a6ea471fc44edec0b91e90a2b0500fd05befa9c1
LJThao <yengfong.thao@atlasschool.com>
2024-09-03 01:14:44 -0500

    task 4

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Plotting a histogram"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """Plotting a histogram of student scores"""
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+    # plotting a histogram
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.title("Project A")
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    plt.show()
