312c1d5875de2512bcd4724a47f5903e3fe7227e
LJThao <yengfong.thao@atlasschool.com>
2024-09-03 03:23:17 +0000

    task 12

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Function that performs add, sub, multi, div"""
+
+
+def np_elementwise(mat1, mat2):
+    """Returns a tuple containing all elements"""
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
