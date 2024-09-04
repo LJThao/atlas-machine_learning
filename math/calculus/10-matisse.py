3ac72c46036a53dcfc48bf8018c06aa9e2a2997a
LJThao <yengfong.thao@atlasschool.com>
2024-09-04 11:43:01 -0500

    task 10

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,14 @@
+#!/bin/usr/env python3
+"""Function to calculate derivative of a poly"""
+
+
+def poly_derivative(poly):
+    """Calculate derivative of a poly"""
+    # if the the poly list is empty, then return
+    if not poly:
+        return None
+    # if the poly has one element then return as derivative
+    elif len(poly) == 1:
+        return [0]
+    # calculates the derivative by multiplying, then skip constant x^0
+    return [coef * i for i, coef in enumerate(poly) if i > 0]
