7c7ca4a631abeb22859f2251519553c59c0333d8
LJThao <yengfong.thao@atlasschool.com>
2024-09-09 23:37:15 +0000

    update class poisson

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -20,3 +20,22 @@ class Poisson():
                 raise ValueError("data must contain multiple values")
             # calculate the lambtha
             self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """Calculate the value of PMF for a given number of
+        sucessess using the posson distribution"""
+        # setting approximation of constant
+        e = 2.7182818285
+        # check if k is less, then return
+        if k < 0:
+            return 0
+        # check if k is an integer, convert if it isn't
+        if not isinstance(k, int):
+            k = int(k)
+        # calulate the factorial of k!
+        fact = 1
+        if k > 1:
+            for i in range(2, k + 1):
+                fact *= i
+        # calculate the # of successes using the poisson formula
+        return (e ** (-self.lambtha)) * (self.lambtha ** k) / (fact)
