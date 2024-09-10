257fcb2ec14d062916cf6e7936664d3a87646138
LJThao <yengfong.thao@atlasschool.com>
2024-09-10 18:13:09 +0000

    divide by len(data) to get avg for var

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -18,7 +18,7 @@ class Normal():
                 raise ValueError("data must contain multiple values")
 
             self.mean = (sum(data)) / (len(data))
-            self.stddev = (sum((x - self.mean) ** 2 for x in data)) ** (1/2)
+            self.stddev = (((sum((x - self.mean) ** 2 for x in data)) / len(data))) ** (1/2)
 
     def z_score(self, x):
         """Calculates the z_score of a given x-value:
