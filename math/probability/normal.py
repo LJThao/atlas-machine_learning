7e40dd5eaa48d14b0f937c9315e4a8ca38c14bd5
LJThao <yengfong.thao@atlasschool.com>
2024-09-10 17:15:04 +0000

    added code to def init

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -6,3 +6,16 @@ class Normal():
     """Class Normal"""
     def __init__(self, data=None, mean=0., stddev=1.):
         """Initiaizing data, mean, stddev"""
+        if data is None:
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            
+            self.mean = (sum(data)) / (len(data))
+            self.stddev = (sum((x - self.mean) ** 2 for x in data)) ** (1/2)
