2b0c9870d8f3a546b0a138bd54b9fa58b0db29cd
LJThao <yengfong.thao@atlasschool.com>
2024-09-10 06:24:48 +0000

    created class and contructor

---
+++ math/probability/exponential.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Class Exponential that represents an exponential
+distribution"""
+
+
+class Exponential():
+    """Class Exponential"""
+    def __init__(self, data=None, lambtha=1.):
+        """Initialize data and lambtha"""
