7ca38886735a83ec8c27101a318622a5416901ec
LJThao <yengfong.thao@atlasschool.com>
2024-09-11 23:42:09 +0000

    added code to class

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -6,3 +6,24 @@ class Binomial():
     """Class Binomial"""
     def __init__(self, data=None, n=1, p=0.5):
         """Initializing data, n, p"""
+        if data is None:
+            if n < 1:
+                raise ValueError("n must be a positive value")
+            if not (0 < p < 1):
+                raise ValueError("p must be greater than 0 and less than 1")
+            self.n = n
+            self.p = p
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+            # Calculating n and p from data
+            p = sum(data) / len(data)
+            n = round(sum(data) / p)
+            p = sum(data) / (n * len(data))
+
+        # making sure n is an int and p is a float
+        self.n = int(n)
+        self.p = float(p)
