d9b4b5a313d537b1103fcc6dafc3aa3baa4785aa
LJThao <yengfong.thao@atlasschool.com>
2024-09-24 04:29:48 +0000

    import and class

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""Class Neuron that defines a single
+neuron performing binary classification"""
+import numpy as np
+
+
+class Neuron():
+    """Class Neuron"""
+    def __init__(self, nx):
+        """Class Constructor"""
+
+    
