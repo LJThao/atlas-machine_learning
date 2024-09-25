25c8280cc1ba0377cc7ab0351f274b34cfaa7186
LJThao <yengfong.thao@atlasschool.com>
2024-09-24 23:47:49 -0500

    copy file 10 to 12, 13, 14, 15

---
+++ supervised_learning/classification/14-neural_network.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""Class NeuralNetwork that defines a neural network with a hidden
+layer performing binary classification based on 13-neural_network.py"""
+import numpy as np
+
+
+class NeuralNetwork():
+    """Class NeuralNetwork"""
+    def __init__(self, nx, nodes):
+        """Class constructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        # setting private instance attributes
+        self.__W1 = np.random.normal(size=(nodes, nx))
+        self.__b1 = np.zeros(shape=(nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.normal(size=(1, nodes))
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """use getter method for W1"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """use getter method for b1"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """use getter method for a1"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """use getter method for w2"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """use getter method for b2"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """use getter method for a2"""
+        return self.__A2
+
+    def forward_prop(self, X):
+        """calculating the forward propagation of the neural network:
+
+        X = input data, nx = number of input features of a neuron,
+        m = number of examples
+
+        """
+        self.__A1 = 1 / (1 + np.exp(-(np.dot(self.W1, X) + self.b1)))
+        self.__A2 = 1 / (1 + np.exp(-(np.dot(self.W2, self.A1) + self.b2)))
+        return self.A1, self.A2
