5393b69bb1c6518402a1f487d135739c90f59d57
LJThao <yengfong.thao@atlasschool.com>
2024-09-24 23:21:10 -0500

    move 8 file to 9

---
+++ supervised_learning/classification/9-neural_network.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Class NeuralNetwork that defines a neural network with a hidden
+layer performing binary classification based on 8-neural_network.py"""
+import numpy as np
+
+
+class NeuralNetwork():
+    """Class NeuralNetwork"""
+    def __init__(self, nx, nodes):
+        """Class constructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.W1 = np.random.normal(size=(nodes, nx))
+        self.b1 = np.zeros(shape=(nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.normal(size=(1, nodes))
+        self.b2 = 0
+        self.A2 = 0
