986cd8e4e076f8d5b717d8178713ed8ef98c0810
LJThao <yengfong.thao@atlasschool.com>
2024-09-25 13:52:23 -0500

    added ifs and for loop

--- supervised_learning/classification/14-neural_network.py
+++ supervised_learning/classification/14-neural_network.py
@@ -100,3 +100,15 @@ class NeuralNetwork():
     def train(self, X, Y, iterations=5000, alpha=0.05):
         """trains the neural network and returning the evaluation of the training
         data after iterations of training have occurred"""
+        if not isinstance(iterations, int):
+            raise TypeError("iterations must be an integer")
+        if iterations <= 0:
+            raise ValueError("iterations must be a positive integer")
+        if not isinstance(alpha, float):
+            raise TypeError("alpha must be a float")
+        if alpha <= 0:
+            raise ValueError("alpha must be positive")
+        for i in range(iterations):
+            self.forward_prop(X)
+            self.gradient_descent(X, Y, self.A1, self.A2, alpha)
+        return self.evaluate(X, Y)
