b7ca61ca347e8ca330a03569b40fc17b91c4ec5a
LJThao <yengfong.thao@atlasschool.com>
2024-09-24 22:44:18 -0500

    remove or

--- supervised_learning/classification/6-neuron.py
+++ supervised_learning/classification/6-neuron.py
@@ -105,7 +105,7 @@ class Neuron():
             raise TypeError("iterations must be an integer")
         if iterations <= 0:
             raise ValueError("iterations must be a positive integer")
-        if not isinstance(alpha, float) or alpha <= 0:
+        if not isinstance(alpha, float):
             raise TypeError("alpha must be a float")
         if alpha <= 0:
             raise ValueError("alpha must be positive")
@@ -113,3 +113,4 @@ class Neuron():
             A = self.forward_prop(X)
             self.gradient_descent(X, Y, A, alpha)
         return self.evaluate(X, Y)
+    
