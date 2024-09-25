7b870b735dc625d23578b50fc8da255275dc226b
LJThao <yengfong.thao@atlasschool.com>
2024-09-24 23:11:45 -0500

    class neuralnetwork

---
+++ supervised_learning/classification/8-neural_network.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Class NeuralNetwork that defines a neural network with a hidden
+layer performing binary classification"""
+import numpy as np
+
+
+class NeuralNetwork():
+    """Class NeuralNetwork"""
+    def __init__(self, nx, nodes):
+        """Class constructor"""
+        
