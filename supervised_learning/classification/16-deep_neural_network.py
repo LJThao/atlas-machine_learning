da9cef9f2a89a0f8cde313afbed57f8ea35e57a7
LJThao <yengfong.thao@atlasschool.com>
2024-09-25 14:52:46 -0500

    adding class deepneuralnetwork

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,11 @@
+#1/usr/bin/env python3
+"""Class DeepNeuralNetwork that defines a deep neural
+network performing binary classification"""
+import numpy as np
+
+
+class DeepNeuralNetwork():
+    """Class DeepNeuralNetwork"""
+    def __init__(self, nx, layers):
+        """Class constructor"""
+        
