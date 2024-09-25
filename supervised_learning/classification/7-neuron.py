59e7e1900f6d89660c4696a1e09781b11dce0080
LJThao <yengfong.thao@atlasschool.com>
2024-09-24 22:43:48 -0500

    remove or

--- supervised_learning/classification/7-neuron.py
+++ supervised_learning/classification/7-neuron.py
@@ -105,7 +105,7 @@ class Neuron():
             raise TypeError("iterations must be an integer")
         if iterations <= 0:
             raise ValueError("iterations must be a positive integer")
-        if not isinstance(alpha, float) or alpha <= 0:
+        if not isinstance(alpha, float):
             raise TypeError("alpha must be a float")
         if alpha <= 0:
             raise ValueError("alpha must be positive")
