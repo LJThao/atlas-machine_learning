9844cfa1b8e0b98ea63fe7479d5e7905fc860457
LJThao <yengfong.thao@atlasschool.com>
2024-09-25 19:14:56 -0500

    added def gradient_des

--- supervised_learning/classification/21-deep_neural_network.py
+++ supervised_learning/classification/21-deep_neural_network.py
@@ -48,3 +48,49 @@ class DeepNeuralNetwork():
     def weights(self):
         """use getter method for weights"""
         return self.__weights
+    
+    def forward_prop(self, X):
+        """calculates the forward propagation of the neural
+        network"""
+        # using the sigmoid function
+        def sig(Z):
+            """sigmoid function"""
+            return 1 / (1 + np.exp(-Z))
+
+        # X saved to the cache dictionary using key A0
+        self.__cache['A0'] = X
+
+        # iterating each layer by applying W, b, Z
+        A = X
+        for i in range(1, self.__L + 1):
+            W = self.__weights[f'W{i}']
+            b = self.__weights[f'b{i}']
+            Z = np.dot(W, A) + b
+            A = sig(Z)
+            self.__cache[f'A{i}'] = A
+
+        # returning output and the cache that was updated
+        return A, self.__cache
+
+    def cost(self, Y, A):
+        """calculates the cost of the model using a logistic
+        regression"""
+        log_A = np.log(A)
+        log_1_minus_A = np.log(1.0000001 - A)
+        loss = Y * log_A + (1 - Y) * log_1_minus_A
+
+        # calculate the average cost
+        return -np.sum(loss) / A.shape[1]
+
+    def evaluate(self, X, Y):
+        """evaluates the neural network's predictions and returns
+        the neuron's prediction and the cost of the network"""
+        A, _ = self.forward_prop(X)
+        prediction = (A >= 0.5).astype(int)
+        cost = self.cost(Y, A)
+        return prediction, cost
+
+    def gradient_descent(self, Y, cache, alpha=0.05):
+        """calculates one pass of gradient descent on the neural
+        network"""
+
