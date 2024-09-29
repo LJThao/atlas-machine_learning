708b735b85a1fab6cc89bc74724308a7569a38f2
LJThao <yengfong.thao@atlasschool.com>
2024-09-29 12:48:44 -0500

    added placeholder func

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -3,3 +3,11 @@
 import tensorflow.compat.v1 as tf
 
 
+def create_placeholders(nx, classes):
+    """returns two placeholders x and y
+    for the neural network
+    
+    nx = number of feature columns in the data
+    classes = number of classes in the classifier
+    
+    """
