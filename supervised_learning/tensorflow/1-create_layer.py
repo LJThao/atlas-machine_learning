217b1da95944b4c03649be141eeb1fe4c3b99c3d
LJThao <yengfong.thao@atlasschool.com>
2024-09-30 08:22:04 -0500

    add def create_layer

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -3,3 +3,13 @@
 import tensorflow.compat.v1 as tf
 
 
+def create_layer(prev, n, activation):
+    """returns the tensor output of the layer:
+    
+    prev = the previous layer of the tensor output
+    n = number of nodes in the layer to create
+    activation = activation function that the layer should
+    use
+    
+    """
+    
