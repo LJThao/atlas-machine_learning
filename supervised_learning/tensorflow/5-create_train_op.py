89950ceb74f2410999ddd27cf63f336b44bd3c77
LJThao <yengfong.thao@atlasschool.com>
2024-09-30 21:16:07 -0500

    created def, docstring

--- supervised_learning/tensorflow/5-create_train_op.py
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -3,3 +3,11 @@
 import tensorflow.compat.v1 as tf
 
 
+def create_train_op(loss, alpha):
+    """creates the training operation for the network:
+    
+    loss = the loss of the network's prediction
+    alpha = the learning rate
+    
+    """
+    
