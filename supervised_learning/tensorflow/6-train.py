0005ba8789f1df7a313e234f16fc9b3e78ac02c4
LJThao <yengfong.thao@atlasschool.com>
2024-09-30 21:46:59 -0500

    add def and docstring

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -8,3 +8,16 @@ create_train_op = __import__('5-create_train_op').create_train_op
 forward_prop = __import__('2-forward_prop').forward_prop
 
 
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
+    """builds, trains, and saves a neural network classifier:
+
+    X_train = numpy.ndarry containing the training input data
+    X_valid = numpy.ndarray containing the validation input data
+    Y_valid = numpy.ndarray containing the validation labels
+    layer_sizes = list containing the number of nodes in each layer of the network
+    activations = list containing the activation functions for each layer of the network
+    alpha = the learning rate
+    iterations = the number of iterations to train over
+    save_path = designates where to save the model
+
+    """
