a9164765448dd3aa597775463fbf8dba2d9fed82
LJThao <yengfong.thao@atlasschool.com>
2024-09-30 21:42:01 -0500

    pycode

--- supervised_learning/tensorflow/5-create_train_op.py
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -5,10 +5,10 @@ import tensorflow.compat.v1 as tf
 
 def create_train_op(loss, alpha):
     """creates the training operation for the network:
-    
+
     loss = the loss of the network's prediction
     alpha = the learning rate
-    
+
     """
     # create optimizer using the gradient descent with alpha
     opt_op = tf.compat.v1.train.GradientDescentOptimizer(alpha)
