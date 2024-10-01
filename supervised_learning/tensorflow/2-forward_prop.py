6e30aa10830cd2f479437f4e2bb29d38ff2c5d1e
LJThao <yengfong.thao@atlasschool.com>
2024-09-30 19:04:59 -0500

    add more to docstring

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -5,4 +5,13 @@ import tensorflow.compat.v1 as tf
 
 def forward_prop(x, layer_sizes=[], activations=[]):
     """returns the prediction of the network in tensor
-    form"""
+    form:
+    
+    x = placeholder for the input data
+    layer_sizes = list containing the number of nodes in
+    each layer of the network
+    activations = list containing the activation functions for
+    each layer of the network
+    
+    """
+    
