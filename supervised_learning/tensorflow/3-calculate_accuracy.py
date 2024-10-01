39ad6ae8c4718dd16dce9cb7e1667ccd479e5a0a
LJThao <yengfong.thao@atlasschool.com>
2024-09-30 19:55:37 -0500

    add more to docstring

--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -4,5 +4,9 @@ import tensorflow.compat.v1 as tf
 
 
 def calculate_accuracy(y, y_pred):
-    """calculates the accuracy of a prediction"""
+    """calculates the accuracy of a prediction:
     
+    y = placeholder for the labels of the input data
+    y_pred = a tensor containing the network's predictions
+    
+    """
