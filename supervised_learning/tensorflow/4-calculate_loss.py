1cacee83661b9ea655fa793e5f554a524e7aaf45
LJThao <yengfong.thao@atlasschool.com>
2024-09-30 21:12:48 -0500

    pycode

--- supervised_learning/tensorflow/4-calculate_loss.py
+++ supervised_learning/tensorflow/4-calculate_loss.py
@@ -6,10 +6,10 @@ import tensorflow.compat.v1 as tf
 def calculate_loss(y, y_pred):
     """calculates the softmax cross-entropy loss
     of a prediction
-    
+
     y = placeholder for the labels of the input data
     y_pred = tensor containing the network's predictions
-    
+
     """
     # calculating the soft max cross-entropy loss
     tensor = tf.compat.v1.losses.softmax_cross_entropy(y, y_pred)
