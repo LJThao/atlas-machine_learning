e2f0e3611f3d7adc893045306f3b37272e76f852
LJThao <yengfong.thao@atlasschool.com>
2024-09-30 20:55:01 -0500

    add func and main

--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -10,3 +10,12 @@ def calculate_accuracy(y, y_pred):
     y_pred = a tensor containing the network's predictions
     
     """
+    # converting one-hot encoded true and predicted labels
+    y = tf.argmax(y, axis=1)
+    y_pred = tf.argmax(y_pred, axis=1)
+    # comparing the prediction to the labels
+    prediction = tf.equal(y, y_pred)
+    # casting boolean values to floats then calculates the mean
+    tensor = tf.reduce_mean(tf.cast(prediction, tf.float32))
+    # returns a tensor of the accuracy of a prediction
+    return tensor
