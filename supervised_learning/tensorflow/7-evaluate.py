3dc15db9f26ab0c44c221b1ba976ab15c3ecc2ee
LJThao <yengfong.thao@atlasschool.com>
2024-10-02 02:29:53 -0500

    def evaluate, docstring

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -3,3 +3,12 @@
 import tensorflow.compat.v1 as tf
 
 
+def evaluate(X, Y, save_path):
+    """Evaluates the output of a neural network:
+    
+    X = numpy.ndarray containing the input data to evaluate
+    Y = numpy.ndarray containing the one-hot labels for X
+    save_path = the location to load the model from
+    
+    """
+    
