361983baf86df04462ae5b77df95d237505c91de
LJThao <yengfong.thao@atlasschool.com>
2024-10-09 10:53:35 -0500

    function and docstring

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 """Shuffle Function"""
+import numpy as np
+
+
+def shuffle_data(X, Y):
+    """Function that shuffles the data points in two
+    matrices the same way:
+
+    X = the first numpy.ndarray of shape (m, nx) to shuffle
+        -> m = the number of data points
+        -> nx = the number of features in X
+    Y = the second numpy.ndarray of shape (m, ny) to shuffle
+        -> m = the same number of data points as in X
+        -> ny = the number of features in Y
+
+    """
+    
