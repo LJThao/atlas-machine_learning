02df8c9e0a14b5f1812861213bdf90d327b8b303
LJThao <yengfong.thao@atlasschool.com>
2024-10-09 11:12:37 -0500

    docstring

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -4,3 +4,16 @@ import numpy as np
 
 
 def create_mini_batches(X, Y, batch_size):
+    """Function that that creates mini-batches to be used for training
+    a neural network using mini-batch gradient descent:
+
+    X = a numpy.ndarray of shape (m, nx) representing input data
+        -> m = the number of data points
+        -> nx = the number of features in X
+    Y = a numpy.ndarray of shape (m, ny) representing the labels
+        -> m = the same number of data points as in X
+        -> ny = the number of classes for classification tasks.
+    batch_size = the number of data points in a batch
+
+    """
+    
