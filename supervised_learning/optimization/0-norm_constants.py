486f737674f660f3b99104dff79945ea83af749e
LJThao <yengfong.thao@atlasschool.com>
2024-10-08 22:56:38 -0500

    added def and docstring

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -3,3 +3,15 @@
 import numpy as np
 
 
+def normalization_constants(X, m, s):
+    """Function that normalizes(standardizes) a matrix:
+    
+    X = the numpy.ndarray of shape (d, nx) to normalize
+    ->    d = number of data points
+    ->    nx = number of features
+    m = numpy.ndarray of shape (nx,), contains the mean of all features of X
+    s = numpy.ndarray of shape (nx,), contains the standard deviation of all
+    features of X
+    
+    """
+    
