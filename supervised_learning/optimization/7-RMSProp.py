f25f82fa32c79bb342863b04b8a2fa02422355c5
LJThao <yengfong.thao@atlasschool.com>
2024-10-09 16:14:20 -0500

    func and docstring

--- supervised_learning/optimization/7-RMSProp.py
+++ supervised_learning/optimization/7-RMSProp.py
@@ -3,3 +3,15 @@
 import numpy as np
 
 
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """Function that updates a variable using the RMSProp optimization
+    algorithm:
+
+    alpha = the learning rate
+    beta2 = the RMSProp weight
+    epsilon = a small number to avoid division by zero
+    var = a numpy.ndarray containing the variable to be updated
+    grad = a numpy.ndarray containing the gradient of var
+    s = the previous second moment of var
+
+    """
