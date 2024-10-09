f902d999cf83b7fd67ad853b991a0d3d0e280c6e
LJThao <yengfong.thao@atlasschool.com>
2024-10-09 12:56:50 -0500

    func and docstring

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -3,3 +3,10 @@
 import numpy as np
 
 
+def moving_average(data, beta):
+    """Function that calculates the weighted moving average of a data set:
+
+    data = the list of data to calculate the moving average of
+    beta = the weight used for the moving average
+
+    """
