541738f370a35a9a88bcc122c59f17692384ec83
LJThao <yengfong.thao@atlasschool.com>
2024-10-09 13:31:30 -0500

    func and docstring

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -3,3 +3,15 @@
 import numpy as np
 
 
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """Function that updates a variable using the gradient descent
+     with momentum optimization algorithm:
+
+    alpha = the learning rate
+    beta1 = the momentum weight
+    var = a numpy.ndarray containing the variable to be updated
+    grad = a numpy.ndarray containing the gradient of var
+    v = the previous first moment of var
+
+    """
+    
