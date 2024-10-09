1cf0d18406b52bde4354693de339c75d4cddd306
LJThao <yengfong.thao@atlasschool.com>
2024-10-08 23:18:02 -0500

    function for constants and normalize, main files

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -3,15 +3,19 @@
 import numpy as np
 
 
-def normalization_constants(X, m, s):
-    """Function that normalizes(standardizes) a matrix:
+def normalization_constants(X):
+    """Function that calculates the normalization (standardization)
+    constants of a matrix:
     
-    X = the numpy.ndarray of shape (d, nx) to normalize
-    ->    d = number of data points
+    X = the numpy.ndarray of shape (m, nx) to normalize
+    ->    m = number of data points
     ->    nx = number of features
-    m = numpy.ndarray of shape (nx,), contains the mean of all features of X
-    s = numpy.ndarray of shape (nx,), contains the standard deviation of all
-    features of X
     
     """
-    
+    # calculates the mean of each column in the matrix
+    mean = np.mean(X, axis=0)
+    # calculates the std of each column
+    std = np.std(X, axis=0)
+
+    # returns the mean and std of each column
+    return (mean, std)
