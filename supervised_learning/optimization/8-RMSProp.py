09f0275b2e830cc62a1fb972695a69a024999368
LJThao <yengfong.thao@atlasschool.com>
2024-10-09 16:58:54 -0500

    rmsprop func and docstring

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -3,3 +3,13 @@
 import tensorflow as tf
 
 
+def create_RMSProp_op(alpha, beta2, epsilon):
+    """Function that sets up the RMSProp optimization algorithm
+    in TensorFlow:
+
+    alpha = the learning rate
+    beta2 = the RMSProp weight (Discounting factor)
+    epsilon = a small number to avoid division by zero
+
+    """
+    
