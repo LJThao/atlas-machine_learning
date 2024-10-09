76d9ebc535ac523c0818ce798ea996a26fb8842a
LJThao <yengfong.thao@atlasschool.com>
2024-10-09 18:00:52 -0500

    func and docstring

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -3,3 +3,19 @@
 import numpy as np
 
 
+def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
+    """Function that updates a variable in place using the Adam optimization
+    algorithm:
+
+    alpha = the learning rate
+    beta1 = the weight used for the first moment
+    beta2 = the weight used for the second moment
+    epsilon = a small number to avoid division by zero
+    var = a numpy.ndarray containing the variable to be updated
+    grad = a numpy.ndarray containing the gradient of var
+    v = the previous first moment of var
+    s = the previous second moment of var
+    t = the time step used for bias correction
+
+    """
+    
