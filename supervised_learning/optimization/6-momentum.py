93605445ff53733bbd0a74b6b50896c28fbd74b7
LJThao <yengfong.thao@atlasschool.com>
2024-10-09 13:49:13 -0500

    func and docstring

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -3,3 +3,12 @@
 import numpy as np
 
 
+def create_momentum_op(alpha, beta1):
+    """Function that sets up the gradient descent with momentum
+    optimization algorithm in TensorFlow:
+
+    alpha = the learning rate.
+    beta1 = the momentum weight.
+
+    """
+    
