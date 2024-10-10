05efb20d8f60b73bc9dc7a2183d742177624a80f
LJThao <yengfong.thao@atlasschool.com>
2024-10-09 20:32:39 -0500

    func, docstring

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -3,3 +3,13 @@
 import tensorflow as tf
 
 
+def create_batch_norm_layer(prev, n, activation):
+    """Function that creates a batch normalization layer for
+    a neural network in tensorflow:
+
+    prev = the activated output of the previous layer
+    n = the number of nodes in the layer to be created
+    activation = the activation function that should be used on the
+    output of the layer
+
+    """
