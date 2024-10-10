8789db89ef95ce51afdf824bf431117abf98ddd9
LJThao <yengfong.thao@atlasschool.com>
2024-10-09 19:42:17 -0500

    adam upgraded fuc

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -13,3 +13,11 @@ def create_Adam_op(alpha, beta1, beta2, epsilon):
     epsilon = a small number to avoid division by zero
 
     """
+    # use keras API to create Adam Optimizer with the parameters
+    adam_op = tf.keras.optimizers.Adam(learning_rate=alpha,
+                                       beta_1=beta1,
+                                       beta_2=beta2,
+                                       epsilon=epsilon)
+
+    # returns optimizer
+    return (adam_op)
