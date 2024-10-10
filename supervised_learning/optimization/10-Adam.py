7c2189feda607665c51e8eca98e4429363bc9fb8
LJThao <yengfong.thao@atlasschool.com>
2024-10-09 19:35:02 -0500

    func, docstring

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -3,3 +3,13 @@
 import tensorflow as tf
 
 
+def create_Adam_op(alpha, beta1, beta2, epsilon):
+    """Function that sets up the Adam optimization algorithm
+    in TensorFlow:
+
+    alpha = the learning rate
+    beta1 = the weight used for the first moment
+    beta2 = the weight used for the second moment
+    epsilon = a small number to avoid division by zero
+
+    """
