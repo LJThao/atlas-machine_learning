061c59c8149f2d13ece2fecfc2ad1e2608b498dc
LJThao <yengfong.thao@atlasschool.com>
2024-10-09 20:00:06 -0500

    func, docstring

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -3,3 +3,14 @@
 import tensorflow as tf
 
 
+def learning_rate_decay(alpha, decay_rate, decay_step):
+    """Function that creates a learning rate decay operation
+    in tensorflow using inverse time decay:
+
+    alpha = the original learning rate
+    decay_rate = weight used to determine the rate at which alpha will decay
+    decay_step = number of passes of gradient descent that should occur before
+    alpha is decayed further
+
+    """
+    
