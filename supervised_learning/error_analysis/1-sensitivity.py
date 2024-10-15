aeb3c2a162bed6a37a65f7c1de0baf969bd1e0e3
LJThao <yengfong.thao@atlasschool.com>
2024-10-15 17:41:01 -0500

    func, docstring

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -3,3 +3,14 @@
 import numpy as np
 
 
+def sensitivity(confusion):
+    """Function that calculates the sensitivity for each class
+    in a confusion matrix:
+
+    confusion = a confusion numpy.ndarray of shape (classes, classes)
+    where row indices represent the correct labels and column indices
+    represent the predicted labels
+        -> classes is the number of classes
+
+    """
+    
