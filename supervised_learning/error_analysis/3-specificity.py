839d07231e88d65c0ad671265025f0ae23a5cdb4
LJThao <yengfong.thao@atlasschool.com>
2024-10-15 18:50:09 -0500

    func, docstring

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -3,3 +3,13 @@
 import numpy as np
 
 
+def specificity(confusion):
+    """Function that calculates the specificity for each class
+    in a confusion matrix:
+
+    confusion is a confusion numpy.ndarray of shape (classes, classes) where
+    row indices represent the correct labels and column indices represent the predicted labels
+        -> classes is the number of classes
+
+    """
+    
