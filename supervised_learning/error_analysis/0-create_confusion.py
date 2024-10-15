4d1beed363b8ee24e0674a17e498f91f48eb72d6
LJThao <yengfong.thao@atlasschool.com>
2024-10-15 12:44:05 -0500

    function, docstring

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -3,3 +3,15 @@
 import numpy as np
 
 
+def create_confusion_matrix(labels, logits):
+    """Function that creates a confusion matrix:
+
+    labels = a one-hot numpy.ndarray of shape (m, classes) containing the
+    correct labels for each data point
+        m = the number of data points
+        classes = the number of classes
+    logits = a one-hot numpy.ndarray of shape (m, classes) containing the
+    predicted labels
+
+    """
+    
