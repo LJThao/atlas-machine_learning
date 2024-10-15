e5a285dc096f065ac2e1109e7d015e0d07b75b15
LJThao <yengfong.thao@atlasschool.com>
2024-10-15 16:59:08 -0500

    update path

--- supervised_learning/error_analysis/0-main.py
+++ supervised_learning/error_analysis/0-main.py
@@ -4,7 +4,7 @@ import numpy as np
 create_confusion_matrix = __import__('0-create_confusion').create_confusion_matrix
 
 if __name__ == '__main__':
-    lib = np.load('labels_logits.npz')
+    lib = np.load('/root/atlas-machine_learning/supervised_learning/classification/data/labels_logits.npz')
     labels = lib['labels']
     logits = lib['logits']
 
