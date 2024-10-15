1ad4bc7f58f9227ab1b0a1d3708f435dd33ec9a2
LJThao <yengfong.thao@atlasschool.com>
2024-10-15 18:40:20 -0500

    func, docstring

--- supervised_learning/error_analysis/2-precision.py
+++ supervised_learning/error_analysis/2-precision.py
@@ -3,3 +3,14 @@
 import numpy as np
 
 
+def precision(confusion):
+    """Function that calculates the precision for each class in a
+    confusion matrix:
+
+    confusion = a confusion numpy.ndarray of shape (classes, classes) where
+    row indices represent the correct labels and column indices represent
+    the predicted labels
+        -> classes is the number of classes
+
+    """
+    
