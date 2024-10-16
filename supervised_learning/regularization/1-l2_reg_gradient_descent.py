8102b30566e43ab7a92d5987315ca2773ff6db12
LJThao <yengfong.thao@atlasschool.com>
2024-10-15 23:56:55 -0500

    l2 rgd func

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -17,4 +17,23 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     L = the number of layers of the network
 
     """
-    
+    # gets the # of training examples from shape of the label mat Y
+    m = Y.shape[1]
+
+    # iterate from the last layer to the first to perform backprop
+    for layer in range(L, 0, -1):
+        # get the A and prev activations from cache
+        A = cache[f'A{layer}']
+        A_prev = cache[f'A{layer - 1}'] if layer > 1 else cache['A0']
+
+        # calculate dZ, use tanh for hidden then softmax for outer layer
+        dZ = (A - Y) if layer == L else dA * (1 - np.square(A))
+        # calculate dW with l2 regularization
+        dW = (np.matmul(dZ, A_prev.T) / m) + (lambtha / m) * weights[f'W{layer}']
+
+        # updates weights and biases using gradient descent
+        weights[f'W{layer}'] -= alpha * dW
+        weights[f'b{layer}'] -= alpha * (np.sum(dZ, axis=1, keepdims=True) / m)
+
+        # calculate dA for the next layer
+        dA = np.matmul(weights[f'W{layer}'].T, dZ)
