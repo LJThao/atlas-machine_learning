b30141dd0634105477dd060aecdbd616383cd750
LJThao <yengfong.thao@atlasschool.com>
2024-10-15 22:51:46 -0500

    l2_reg_cost func

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -15,4 +15,10 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     m = the number of data points used
 
     """
+    # computing the sum of the squared weights the layers
+    l2_sum = sum(np.sum(v ** 2) for k, v in weights.items() if 'W' in k)
+    # calculate the l2 regularization cost
+    reg_cost = (lambtha / (2 * m)) * l2_sum
     
+    # return the cost of the network accounting of L2 regularization
+    return (cost + reg_cost)
