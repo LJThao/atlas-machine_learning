39ac0e1154167ebdabf460cb113fc399a8ecebdc
LJThao <yengfong.thao@atlasschool.com>
2024-10-15 20:15:14 -0500

    func, docstring

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -3,3 +3,13 @@
 import numpy as np
 
 
+def f1_score(confusion):
+    """Function that calculates the F1 score of a confusion matrix:
+
+    confusion is a confusion numpy.ndarray of shape (classes,
+    classes) where row indices represent the correct labels and
+    column indices represent the predicted labels
+    classes = the number of classes
+
+    """
+    
