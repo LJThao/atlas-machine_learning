e8fb768752b5810fe9f0eaafa4a434a8657308ff
LJThao <yengfong.thao@atlasschool.com>
2024-10-15 21:27:55 -0500

    func for f1

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""F1 Score Function"""
+"""F1 Score Module"""
 import numpy as np
 sensitivity = __import__('1-sensitivity').sensitivity
 precision = __import__('2-precision').precision
@@ -14,3 +14,12 @@ def f1_score(confusion):
     classes = the number of classes
 
     """
+    # calculating the sensitivity for each class
+    sensi = sensitivity(confusion)
+    # calculating the precision for each class
+    preci = precision(confusion)
+    # calculating the F1 score for each class
+    f1_score = 2 * (preci * sensi) / (preci + sensi)
+
+    # returns shape (classes,) containing the F1 score of each class
+    return (f1_score)
