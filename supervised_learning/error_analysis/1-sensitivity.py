da206f7ef620407e6675b6761f74bbe19f00df97
LJThao <yengfong.thao@atlasschool.com>
2024-10-15 22:06:59 -0500

    update

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -10,7 +10,7 @@ def sensitivity(confusion):
     confusion = a confusion numpy.ndarray of shape (classes, classes)
     where row indices represent the correct labels and column indices
     represent the predicted labels
-        -> classes is the number of classes
+        -> classes = the number of classes
 
     """
     # getting the true positives for each diagonally from the matrix
