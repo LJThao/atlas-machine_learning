c78af6158681f2971c1826173818c83a9fe89c87
LJThao <yengfong.thao@atlasschool.com>
2024-10-16 00:38:47 -0500

    func, docstring

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -3,3 +3,13 @@
 import tensorflow as tf
 
 
+def l2_reg_cost(cost, model):
+    """Function that calculates the cost of a neural network
+    with L2 regularization:
+
+    cost = a tensor containing the cost of the network without L2
+    regularization
+    model = a Keras model that includes layers with L2 regularization
+
+    """
+    
