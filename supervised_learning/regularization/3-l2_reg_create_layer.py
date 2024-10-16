086b00ea1c5d18d13ef0c9838ff5f3b6d6bc9321
LJThao <yengfong.thao@atlasschool.com>
2024-10-16 17:56:35 -0500

    func, docstring

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -3,3 +3,14 @@
 import tensorflow as tf
 
 
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """Function that creates a neural network layer in tensorFlow that
+    includes L2 regularization:
+
+    prev = a tensor containing the output of the previous layer
+    n = the number of nodes the new layer should contain
+    activation = the activation function that should be used on the layer
+    lambtha = the L2 regularization parameter
+
+    """
+    
