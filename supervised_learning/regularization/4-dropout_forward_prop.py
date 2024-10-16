28e254b7f6338c815468d7ab698fd811ea3d2147
LJThao <yengfong.thao@atlasschool.com>
2024-10-16 18:32:04 -0500

    func, doc

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -3,3 +3,17 @@
 import numpy as np
 
 
+def dropout_forward_prop(X, weights, L, keep_prob):
+    """Function that conducts forward propagation using Dropout:
+
+    X = a numpy.ndarray of shape (nx, m) containing the input data for the network
+    nx = the number of input features
+    m = the number of data points
+    weights = a dictionary of the weights and biases of the neural network
+    L = the number of layers in the network
+    keep_prob = the probability that a node will be kept
+    -> All layers except the last should use the tanh activation function
+    -> The last layer should use the softmax activation function
+
+    """
+    
