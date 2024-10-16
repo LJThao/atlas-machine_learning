ce2bd3b9d22a5b0225112e314a6c81a6e3c52981
LJThao <yengfong.thao@atlasschool.com>
2024-10-15 20:12:41 -0500

    func, main

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -8,8 +8,21 @@ def specificity(confusion):
     in a confusion matrix:
 
     confusion is a confusion numpy.ndarray of shape (classes, classes) where
-    row indices represent the correct labels and column indices represent the predicted labels
+    row indices represent the correct labels and column indices represent the
+    predicted labels
         -> classes is the number of classes
 
     """
-    
+    # getting the true positives for each class diagonally from the matrix
+    true_pos = np.diag(confusion)
+    # calculating the false positives: sum then subtract the true positives
+    false_pos = np.sum(confusion, axis=0) - true_pos
+    # calculating the false negatives for each class
+    false_neg = np.sum(confusion, axis=1) - true_pos
+    # calculating the true negatives for each class
+    true_neg = np.sum(confusion) - (true_pos + false_neg + false_pos)
+    # calculating the specificity for each class
+    specificity = true_neg / (true_neg + false_pos)
+
+    # returns the shape (classes,) containing the specificity of each class
+    return (specificity)
