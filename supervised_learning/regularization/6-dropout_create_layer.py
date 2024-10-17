d58170dd7e805e0255b56b5c026a43ead8ba31df
LJThao <yengfong.thao@atlasschool.com>
2024-10-16 20:14:54 -0500

    dropout create layer func

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -13,4 +13,21 @@ def dropout_create_layer(prev, n, activation, keep_prob,training=True):
     training = a boolean indicating whether the model is in training mode
 
     """
-    
+    # init the layer weights using variance scaling
+    init_weights = tf.keras.initializers.VarianceScaling(scale=2.0,
+                                                         mode="fan_avg")
+
+    # creates a layer
+    layer = tf.keras.layers.Dense(units=n,
+                                  activation=activation,
+                                  kernel_initializer=init_weights)
+
+    # apply layer to the prev layer
+    layer_out = layer(prev)
+
+    # apply dropout to the layer output
+    dropout_layer = tf.keras.layers.Dropout(rate=1 - keep_prob)
+    layer_out = dropout_layer(layer_out, training=training)
+
+    # returns the output
+    return (layer_out)
