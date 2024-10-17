5325ac6ff339aaba5f6fcf633b052c4381edf580
LJThao <yengfong.thao@atlasschool.com>
2024-10-16 20:17:00 -0500

    pycode

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -3,7 +3,7 @@
 import tensorflow as tf
 
 
-def dropout_create_layer(prev, n, activation, keep_prob,training=True):
+def dropout_create_layer(prev, n, activation, keep_prob, training=True):
     """Function that creates a layer of a neural network using dropout:
 
     prev = a tensor containing the output of the previous layer
