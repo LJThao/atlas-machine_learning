0e8b6db62d550e29a8697c8075a5cf85b982a424
LJThao <yengfong.thao@atlasschool.com>
2024-10-22 13:46:04 -0500

    func, docstring

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -3,3 +3,14 @@
 import tensorflow.keras as K
 
 
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """Function that builds a neural network with the Keras library:
+
+    nx = the number of input features to the network
+    layers = a list containing the number of nodes in each layer of the
+    network
+    activations = a list containing the activation functions used for each
+    layer of the network
+    lambtha = the L2 regularization parameter
+    keep_prob = the probability that a node will be kept for dropout
+    """
