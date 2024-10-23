11f3a0281cceb8a6e8188900074aaef64a83df91
LJThao <yengfong.thao@atlasschool.com>
2024-10-23 11:57:58 -0500

    train model update func

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -12,4 +12,14 @@ def train_model(network, data, labels, batch_size, epochs,
     validation_data = the data to validate the model with, if not None
 
     """
-    
+    # trains the model using keras fit function -- now added validation data
+    history_obj = network.fit(data,
+                              labels,
+                              batch_size=batch_size,
+                              epochs=epochs,
+                              verbose=verbose,
+                              shuffle=shuffle,
+                              validation_data=validation_data)
+
+    # returns the history object generated after training
+    return (history_obj)
