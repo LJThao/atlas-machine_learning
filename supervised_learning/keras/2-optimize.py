7734e704ce47c2d43b9a000fa4e8b602e5e74b77
LJThao <yengfong.thao@atlasschool.com>
2024-10-23 11:10:18 -0500

    opt model func

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -13,3 +13,8 @@ def optimize_model(network, alpha, beta1, beta2):
     beta2 = the second Adam optimization parameter
 
     """
+    # compile the model using adam optimizer, loss, and metrics
+    adam_opt = network.compile(optimizer=K.optimizers.Adam(
+        lr=alpha, beta_1=beta1, beta_2=beta2),
+        loss="categorical_crossentropy",
+        metrics=['accuracy'])
