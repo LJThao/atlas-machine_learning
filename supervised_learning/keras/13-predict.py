d721d52ac9f61c97d94178350d98ae789480c0c2
LJThao <yengfong.thao@atlasschool.com>
2024-10-23 15:57:04 -0500

    func, docstr

--- supervised_learning/keras/13-predict.py
+++ supervised_learning/keras/13-predict.py
@@ -3,3 +3,14 @@
 import tensorflow.keras as K
 
 
+def predict(network, data, verbose=False):
+    """Function that makes a prediction using a neural network:
+
+    network = the network model to make the prediction with
+    data = the input data to make the prediction with
+    verbose = a boolean that determines if output should be printed
+    during the prediction process
+    Returns: the prediction for the data
+
+    """
+    
