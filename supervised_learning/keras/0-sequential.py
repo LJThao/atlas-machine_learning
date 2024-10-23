266c7941e67ecb9612822030e181ad8d7740f144
LJThao <yengfong.thao@atlasschool.com>
2024-10-23 10:34:34 -0500

    update func

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -20,13 +20,16 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     model.add(K.layers.Dense(layers[0], activation=activations[0],
                              kernel_regularizer=K.regularizers.l2(lambtha),
                              input_shape=(nx,)))
-    # iterate through and adding each dense layer
+    # iterate through the layers
     for layer in range(1, len(layers)):
-        model.add(K.layers.Dense(layers[layer], activation=activations[layer],
-                                 kernel_regularizer=K.regularizers.l2(lambtha)))
-        # apply dropout to hidden layers
-        if layer < len(layers) - 1:
-            model.add(K.layers.Dropout(1 - keep_prob))
+        # adding dropout to the hidden layers
+        model.add(K.layers.Dropout(rate = 1 - keep_prob))
+        # then add the dense layer for current layer
+        model.add(K.layers.Dense(
+            units=layers[layer],
+            activation=activations[layer],
+            kernel_regularizer=K.regularizers.l2(lambtha))
+            )
 
     # returning the model
     return (model)
