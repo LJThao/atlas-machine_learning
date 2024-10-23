c8fcfc566591460462cf501c2ec8d31478afe64f
LJThao <yengfong.thao@atlasschool.com>
2024-10-23 13:29:46 -0500

    funcs, docstrs

--- supervised_learning/keras/11-config.py
+++ supervised_learning/keras/11-config.py
@@ -3,3 +3,21 @@
 import tensorflow.keras as K
 
 
+def save_config(network, filename):
+    """Function saves a model's configuration in JSON format:
+
+    network = the model whose configuration should be saved
+    filename = the path of the file that the configuration should be saved to
+    Returns: None
+
+    """
+
+
+def load_config(filename):
+    """Function loads a model with a specific configuration:
+
+    filename is the path of the file containing the model's configuration
+    in JSON format
+    Returns: the loaded model
+
+    """
