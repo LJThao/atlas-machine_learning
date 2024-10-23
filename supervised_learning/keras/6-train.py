2bf04ce673fb5abd8bf9e736a6adae7cda7adf2b
LJThao <yengfong.thao@atlasschool.com>
2024-10-23 12:06:08 -0500

    func, docstr,

--- supervised_learning/keras/6-train.py
+++ supervised_learning/keras/6-train.py
@@ -4,3 +4,26 @@ Based on 5-train.py"""
 import tensorflow.keras as K
 
 
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False, patience=0,
+                verbose=True, shuffle=False):
+    """Function to also train the model using early stopping:
+
+    early_stopping = a boolean that indicates whether early stopping
+    should be used
+    early stopping should only be performed if validation_data exists
+    early stopping should be based on validation loss
+    patience = the patience used for early stopping
+
+    """
+    # trains the model using keras fit function -- now added validation data
+    history_obj = network.fit(data,
+                              labels,
+                              batch_size=batch_size,
+                              epochs=epochs,
+                              verbose=verbose,
+                              shuffle=shuffle,
+                              validation_data=validation_data)
+
+    # returns the history object generated after training
+    return (history_obj)
