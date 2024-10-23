723608c797a8b043d524363c8c78cf62721ff90a
LJThao <yengfong.thao@atlasschool.com>
2024-10-23 15:48:30 -0500

    func, docstr

--- supervised_learning/keras/12-test.py
+++ supervised_learning/keras/12-test.py
@@ -3,3 +3,14 @@
 import tensorflow.keras as K
 
 
+def test_model(network, data, labels, verbose=True):
+    """Function that tests a neural network:
+
+    network = the network model to test
+    data = the input data to test the model with
+    labels are the correct one-hot labels of data
+    verbose = a boolean that determines if output should be printed during the testing process
+    Returns: the loss and accuracy of the model with the testing data, respectively
+
+    """
+    
