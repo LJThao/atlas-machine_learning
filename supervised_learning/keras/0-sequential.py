61cb937aabe298e64e4b5ff7a9317ce745c294d6
LJThao <yengfong.thao@atlasschool.com>
2024-10-23 10:39:25 -0500

    docstr

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -13,6 +13,7 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     layer of the network
     lambtha = the L2 regularization parameter
     keep_prob = the probability that a node will be kept for dropout
+    ** You are not allowed to use the Input class **
     """
     # init the sequential model
     model = K.models.Sequential()
