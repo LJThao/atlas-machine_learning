cdfd06d6b55d6ad4d090d3678d7af96abe041c64
LJThao <yengfong.thao@atlasschool.com>
2024-10-23 11:50:15 -0500

    func, docstr

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -4,3 +4,12 @@ Based on 4-train.py"""
 import tensorflow.keras as K
 
 
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+
+    """Function to also analyze validation data:
+
+    validation_data = the data to validate the model with, if not None
+
+    """
+    
