9fdd8061dc1b4b8027efdf567e50063728b65cdf
LJThao <yengfong.thao@atlasschool.com>
2024-10-23 13:20:47 -0500

    funcs, docstrs

--- supervised_learning/keras/10-weights.py
+++ supervised_learning/keras/10-weights.py
@@ -1,2 +1,24 @@
 #!/usr/bin/env python3
 """Save and Load Weights Module"""
+import tensorflow.keras as K
+
+
+def save_weights(network, filename, save_format='keras'):
+    """Function saves a model's weights:
+
+    network = the model whose weights should be saved
+    filename = the path of the file that the weights should be saved to
+    save_format = the format in which the weights should be saved
+    Returns: None
+
+    """
+
+def load_weights(network, filename):
+    """Function loads a model's weights:
+
+    network = the model to which the weights should be loaded
+    filename = the path of the file that the weights should be loaded from
+    Returns: None
+
+    """
+    
