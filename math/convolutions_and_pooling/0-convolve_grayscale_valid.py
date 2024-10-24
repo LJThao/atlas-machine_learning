94a0095beecd8a867641a4b898d161328a2ed3b7
LJThao <yengfong.thao@atlasschool.com>
2024-10-23 22:06:20 -0500

    convolve func

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -6,18 +6,38 @@ import numpy as np
 def convolve_grayscale_valid(images, kernel):
     """Function that performs a valid convolution on grayscale images:
 
-    images is a numpy.ndarray with shape (m, h, w) containing multiple
+    images = a numpy.ndarray with shape (m, h, w) containing multiple
     grayscale images
-    m is the number of images
-    h is the height in pixels of the images
-    w is the width in pixels of the images
-    kernel is a numpy.ndarray with shape (kh, kw) containing the kernel
+    m = the number of images
+    h = the height in pixels of the images
+    w = the width in pixels of the images
+    kernel = a numpy.ndarray with shape (kh, kw) containing the kernel
     for the convolution
-    kh is the height of the kernel
-    kw is the width of the kernel
+    kh = the height of the kernel
+    kw = the width of the kernel
     You are only allowed to use two for loops; any other loops of any kind
     are not allowed
     Returns: a numpy.ndarray containing the convolved images
-
-    """
     
+    """
+    # calculate the output
+    m = images.shape[0]
+    kh, kw = kernel.shape
+    output_h, output_w = [
+        m_size - k_size + 1
+        for m_size, k_size in zip(images.shape[1:], kernel.shape)
+    ]
+
+    # init the output with zeros
+    convolved_images = np.zeros((m, output_h, output_w))
+
+    # apply convolution
+    for y in range(output_h):
+        for x in range(output_w):
+            # Apply tensordot function
+            convolved_images[:, y, x] = np.tensordot(
+                images[:, y:y + kh, x:x + kw],
+                kernel, axes=((1, 2), (0, 1)))
+
+    # return a numpy.ndarry containing the convolved images
+    return (convolved_images)
