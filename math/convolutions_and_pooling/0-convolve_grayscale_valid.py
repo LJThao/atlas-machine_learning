ae806065dedd9725175aca45e81217f13349ea6d
LJThao <yengfong.thao@atlasschool.com>
2024-10-23 19:54:25 -0500

    func, docstr

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -3,3 +3,21 @@
 import numpy as np
 
 
+def convolve_grayscale_valid(images, kernel):
+    """Function that performs a valid convolution on grayscale images:
+
+    images is a numpy.ndarray with shape (m, h, w) containing multiple
+    grayscale images
+    m is the number of images
+    h is the height in pixels of the images
+    w is the width in pixels of the images
+    kernel is a numpy.ndarray with shape (kh, kw) containing the kernel
+    for the convolution
+    kh is the height of the kernel
+    kw is the width of the kernel
+    You are only allowed to use two for loops; any other loops of any kind
+    are not allowed
+    Returns: a numpy.ndarray containing the convolved images
+
+    """
+    
