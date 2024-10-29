5d2c268a22d32bba94339f9a0fe69dec3a82cb6e
LJThao <yengfong.thao@atlasschool.com>
2024-10-29 00:22:31 -0500

    pycode

--- supervised_learning/cnn/4-lenet5.py
+++ supervised_learning/cnn/4-lenet5.py
@@ -20,9 +20,11 @@ def lenet5(x, y):
     -> Fully connected layer with 120 nodes
     -> Fully connected layer with 84 nodes
     -> Fully connected softmax output layer with 10 nodes
-    All layers requiring initialization should initialize their kernels with the
-    he_normal initialization method: tf.keras.initializers.VarianceScaling(scale=2.0)
-    All hidden layers requiring activation should use the relu activation function
+    All layers requiring initialization should initialize their kernels with
+    the_normal initialization method: tf.keras.initializers.VarianceScaling(
+    scale=2.0)
+    All hidden layers requiring activation should use the relu activation
+    function
     you may import tensorflow.compat.v1 as tf
     you may NOT use tf.keras only for the he_normal method.
     Returns:
