4d1b5f9cd82569441274673ecd6e92904ed22599
LJThao <yengfong.thao@atlasschool.com>
2024-10-29 00:14:21 -0500

    update doc

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -7,24 +7,31 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     """Function that performs back propagation over a convolutional layer
     of a neural network:
 
-    dZ is a numpy.ndarray of shape (m, h_new, w_new, c_new) containing the partial derivatives with respect to the unactivated output of the convolutional layer
-    m is the number of examples
-    h_new is the height of the output
-    w_new is the width of the output
-    c_new is the number of channels in the output
-    A_prev is a numpy.ndarray of shape (m, h_prev, w_prev, c_prev) containing the output of the previous layer
-    h_prev is the height of the previous layer
-    w_prev is the width of the previous layer
-    c_prev is the number of channels in the previous layer
-    W is a numpy.ndarray of shape (kh, kw, c_prev, c_new) containing the kernels for the convolution
-    kh is the filter height
-    kw is the filter width
-    b is a numpy.ndarray of shape (1, 1, 1, c_new) containing the biases applied to the convolution
-    padding is a string that is either same or valid, indicating the type of padding used
-    stride is a tuple of (sh, sw) containing the strides for the convolution
-    sh is the stride for the height
-    sw is the stride for the width
+    dZ = a numpy.ndarray of shape (m, h_new, w_new, c_new) containing the
+    partial derivatives with respect to the unactivated output of the
+    convolutional layer
+    m = the number of examples
+    h_new = the height of the output
+    w_new = the width of the output
+    c_new = the number of channels in the output
+    A_prev = a numpy.ndarray of shape (m, h_prev, w_prev, c_prev) containing
+    the output of the previous layer
+    h_prev = the height of the previous layer
+    w_prev = the width of the previous layer
+    c_prev = the number of channels in the previous layer
+    W = a numpy.ndarray of shape (kh, kw, c_prev, c_new) containing the
+    kernels for the convolution
+    kh = the filter height
+    kw = the filter width
+    b = a numpy.ndarray of shape (1, 1, 1, c_new) containing the biases
+    applied to the convolution
+    padding = a string that is either same or valid, indicating the type of
+    padding used
+    stride = a tuple of (sh, sw) containing the strides for the convolution
+    sh = the stride for the height
+    sw = the stride for the width
     you may import numpy as np
-    Returns: the partial derivatives with respect to the previous layer (dA_prev), the kernels (dW), and the biases (db), respectively
+    Returns: the partial derivatives with respect to the previous layer
+    (dA_prev), the kernels (dW), and the biases (db), respectively
 
     """
