4f1bb88c96892e12a36bb5019891fe7ab7e21a89
LJThao <yengfong.thao@atlasschool.com>
2024-10-29 00:10:28 -0500

    update docstr

--- supervised_learning/cnn/1-pool_forward.py
+++ supervised_learning/cnn/1-pool_forward.py
@@ -7,20 +7,20 @@ def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
     """Function that performs forward propagation over a pooling layer of a
     neural network:
 
-    A_prev is a numpy.ndarray of shape (m, h_prev, w_prev, c_prev) containing
+    A_prev = a numpy.ndarray of shape (m, h_prev, w_prev, c_prev) containing
     the output of the previous layer
-    m is the number of examples
-    h_prev is the height of the previous layer
-    w_prev is the width of the previous layer
-    c_prev is the number of channels in the previous layer
-    kernel_shape is a tuple of (kh, kw) containing the size of the kernel for
+    m = the number of examples
+    h_prev = the height of the previous layer
+    w_prev = the width of the previous layer
+    c_prev = the number of channels in the previous layer
+    kernel_shape = a tuple of (kh, kw) containing the size of the kernel for
     the pooling
-    kh is the kernel height
-    kw is the kernel width
-    stride is a tuple of (sh, sw) containing the strides for the pooling
-    sh is the stride for the height
-    sw is the stride for the width
-    mode is a string containing either max or avg, indicating whether to perform
+    kh = the kernel height
+    kw = the kernel width
+    stride = a tuple of (sh, sw) containing the strides for the pooling
+    sh = the stride for the height
+    sw = the stride for the width
+    mode = a string containing either max or avg, indicating whether to perform
     maximum or average pooling, respectively
     you may import numpy as np
     Returns: the output of the pooling layer
