2252ad23a991e14099cc387f3e2287784c5c7a83
LJThao <yengfong.thao@atlasschool.com>
2024-10-30 17:17:35 -0500

    pool backward func

--- supervised_learning/cnn/3-pool_backward.py
+++ supervised_learning/cnn/3-pool_backward.py
@@ -31,3 +31,38 @@ def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
     (dA_prev)
 
     """
+    # unpack
+    (m, h_prev, w_prev, c) = A_prev.shape
+    (kh, kw) = kernel_shape
+    (sh, sw) = stride
+    (m, h_new, w_new, c_new) = dA.shape
+
+    # init dA_prev
+    dA_prev = np.zeros_like(A_prev)
+
+    # iterates over each training examples, height, width
+    for i in range(m):
+        for y in range(h_new):
+            for x in range(w_new):
+                # iterates over c and apply backprop for the pooling op
+                for c_ in range(c):
+                    # calculates the start and end positions
+                    y_start = y * sh
+                    y_end = y_start + kh
+                    x_start = x * sw
+                    x_end = x_start + kw
+
+                    # apply backprop for max pooling
+                    if mode == 'max':
+                        pool_slice = A_prev[i, y_start:y_end, x_start:x_end, c_]
+                        mask = (pool_slice == np.max(pool_slice))
+                        dA_prev[i, y_start:y_end, x_start:x_end, c_] += mask * dA[i, y, x, c_]
+
+                    # apply backprop for avg pooling
+                    elif mode == 'avg':
+                        da = dA[i, y, x, c_]
+                        average = da / (kh * kw)
+                        dA_prev[i, y_start:y_end, x_start:x_end, c_] += np.ones((kh, kw)) * average
+
+    # returns the partial derivatives with respect to the previous layer
+    return (dA_prev)
