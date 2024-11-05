9485cfc45b9d1fa4663fa7684f9592a31ba063b4
LJThao <yengfong.thao@atlasschool.com>
2024-11-05 13:25:39 -0600

    remove from

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -7,7 +7,7 @@ def identity_block(A_prev, filters):
     """Function that builds an identity block as described in Deep Residual
     Learning for Image Recognition (2015):
 
-    A_prev = the output from the previous layer
+    A_prev = the output out of the previous layer
     filters = a tuple or list containing F11, F3, F12, respectively:
     F11 = the number of filters in the first 1x1 convolution
     F3 = the number of filters in the 3x3 convolution
