962ed2257f6c826bd2b021952f64296dded286e8
LJThao <yengfong.thao@atlasschool.com>
2024-11-04 23:35:16 -0600

    incep block func

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -24,3 +24,33 @@ def inception_block(A_prev, filters):
     Returns: the concatenated output of the inception block
 
     """
+    # unpack filter tuples
+    F1, F3R, F3, F5R, F5, FPP = filters
+
+    # F1 convolution
+    conv1 = K.layers.Conv2D(F1, (1, 1), padding='same',
+                            activation='relu')(A_prev)
+
+    # F1 then followed by F3
+    conv3 = K.layers.Conv2D(F3R, (1, 1), padding='same',
+                            activation='relu')(A_prev)
+    conv3 = K.layers.Conv2D(F3, (3, 3), padding='same',
+                            activation='relu')(conv3)
+
+    # F1 then followed by F5
+    conv5 = K.layers.Conv2D(F5R, (1, 1), padding='same',
+                            activation='relu')(A_prev)
+    conv5 = K.layers.Conv2D(F5, (5, 5), padding='same',
+                            activation='relu')(conv5)
+
+    # pooling the layers
+    pool = K.layers.MaxPooling2D((3, 3), strides=(1, 1),
+                                 padding='same')(A_prev)
+    pool = K.layers.Conv2D(FPP, (1, 1), padding='same',
+                           activation='relu')(pool)
+
+    # concatenating
+    conc_output = K.layers.Concatenate(axis=-1)([conv1, conv3, conv5, pool])
+
+    # returns the concatenate output of the inception block
+    return (conc_output)
