32495e0c9457ed9145d0302f7448a27c77fdcbc5
LJThao <yengfong.thao@atlasschool.com>
2024-11-05 13:13:02 -0600

    func. docstr

--- supervised_learning/deep_cnns/6-transition_layer.py
+++ supervised_learning/deep_cnns/6-transition_layer.py
@@ -3,3 +3,19 @@
 from tensorflow import keras as K
 
 
+def transition_layer(X, nb_filters, compression):
+    """Function that builds a transition layer as described in
+    Densely Connected Convolutional Networks:
+
+    X = the output from the previous layer
+    nb_filters = an integer representing the number of filters in X
+    compression = the compression factor for the transition layer
+    Your code should implement compression as used in DenseNet-C
+    All weights should use he normal initialization
+    The seed for the he_normal initializer should be set to zero
+    All convolutions should be preceded by Batch Normalization and a
+    rectified linear activation (ReLU), respectively
+    Returns: The output of the transition layer and the number of filters
+    within the output, respectively
+
+    """
