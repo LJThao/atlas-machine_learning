3b7cc83966b141ce62381f9542d5d359fae977b8
LJThao <yengfong.thao@atlasschool.com>
2024-11-05 13:06:58 -0600

    func, doc, imports

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 """ResNet-50 Module"""
 from tensorflow import keras as K
+identity_block = __import__('2-identity_block').identity_block
+projection_block = __import__('3-projection_block').projection_block
 
 
+def resnet50():
+    """Function that builds the ResNet-50 architecture as described
+    in Deep Residual Learning for Image Recognition (2015):
+
+    You can assume the input data will have shape (224, 224, 3)
+    All convolutions inside and outside the blocks should be followed by
+    batch normalization along the channels axis and a rectified linear
+    activation (ReLU), respectively.
+    All weights should use he normal initialization
+    The seed for the he_normal initializer should be set to zero
+    Returns: the keras model
+
+    """
