d79b56bf8aca5b3d6ebf71088bbd7b3af97504f7
LJThao <yengfong.thao@atlasschool.com>
2024-11-05 13:40:04 -0600

    identity block func

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -20,3 +20,32 @@ def identity_block(A_prev, filters):
     Returns: the activated output of the identity block
 
     """
+    # unpack filter tuples
+    F11, F3, F12 = filters
+
+    # init using HeNormal
+    init = K.initializers.HeNormal(seed=0)
+
+    # 1x1 convolution (the first)
+    x1 = K.layers.Conv2D(F11, (1, 1), padding='same',
+                        kernel_initializer=init)(A_prev)
+    x1 = K.layers.BatchNormalization(axis=-1)(x1)
+    x1 = K.layers.Activation('relu')(x1)
+
+    # 3x3 convolution
+    x2 = K.layers.Conv2D(F3, (3, 3), padding='same',
+                        kernel_initializer=init)(x1)
+    x2 = K.layers.BatchNormalization(axis=-1)(x2)
+    x2 = K.layers.Activation('relu')(x2)
+
+    # 1x1 convolution (the second)
+    x3 = K.layers.Conv2D(F12, (1, 1), padding='same',
+                        kernel_initializer=init)(x2)
+    x3 = K.layers.BatchNormalization(axis=-1)(x3)
+
+    # add identity connection and apply activation
+    add_output = K.layers.Add()([x3, A_prev])
+    id_output = K.layers.Activation('relu')(add_output)
+
+    # returns the activated output of the identity block    
+    return (id_output)
