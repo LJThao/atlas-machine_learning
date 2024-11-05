710da70a0e90cd1701c0874dee9697d3ccd0201c
LJThao <yengfong.thao@atlasschool.com>
2024-11-05 14:16:12 -0600

    proj block func

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -15,6 +15,9 @@ def projection_block(A_prev, filters, s=2):
     as the 1x1 convolution in the shortcut connection
     s = the stride of the first convolution in both the main path and
     the shortcut connection
+    main path (m_p) = three convolution layers (1x1, 3x3, 1x1)
+    shortcut (s_c) = contains a 1x1 convolution to match the dimensions with
+    the main output
     All convolutions inside the block should be followed by batch
     normalization along the channels axis and a rectified linear activation
     (ReLU), respectively.
@@ -23,3 +26,35 @@ def projection_block(A_prev, filters, s=2):
     Returns: the activated output of the projection block
 
     """
+    # unpack filter tuples
+    F11, F3, F12 = filters
+
+    # init using HeNormal
+    init = K.initializers.HeNormal(seed=0)
+
+    # main path(m_p) towards the sequence of layers to create new features
+    m_p = K.layers.Conv2D(F11, 1, strides=s,
+                          padding='same',
+                          kernel_initializer=init)(A_prev)
+    m_p = K.layers.BatchNormalization()(m_p)
+    m_p = K.layers.Activation('relu')(m_p)
+    m_p = K.layers.Conv2D(F3, 3, padding='same',
+                          kernel_initializer=init)(m_p)
+    m_p = K.layers.BatchNormalization()(m_p)
+    m_p = K.layers.Activation('relu')(m_p)
+    m_p = K.layers.Conv2D(F12, 1, padding='same',
+                          kernel_initializer=init)(m_p)
+    m_p = K.layers.BatchNormalization()(m_p)
+
+    # shortcut path(s_p) 1x1 to match dimensions
+    s_p = K.layers.Conv2D(F12, 1, strides=s,
+                               padding='same',
+                               kernel_initializer=init)(A_prev)
+    shortcut = K.layers.BatchNormalization()(s_p)
+
+    # applying the shortcut to main and apply relu activation
+    combine_m_s = K.layers.Add()([m_p, shortcut])
+    proj_output = K.layers.Activation('relu')(combine_m_s)
+
+    # returns the activated output of the projection block
+    return (proj_output)
