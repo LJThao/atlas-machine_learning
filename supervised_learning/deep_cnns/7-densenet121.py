f88c579e1ceba14316f25e2daa4e3c5bea8ecbaf
LJThao <yengfong.thao@atlasschool.com>
2024-11-05 13:15:44 -0600

    imports, func, docstr

--- supervised_learning/deep_cnns/7-densenet121.py
+++ supervised_learning/deep_cnns/7-densenet121.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python3
 """DenseNet-121 Module"""
+from tensorflow import keras as K
+dense_block = __import__('5-dense_block').dense_block
+transition_layer = __import__('6-transition_layer').transition_layer
+
+
+def densenet121(growth_rate=32, compression=1.0):
+    """Function that builds the DenseNet-121 architecture as described
+    in Densely Connected Convolutional Networks:
+
+    growth_rate = the growth rate
+    compression = the compression factor
+    You can assume the input data will have shape (224, 224, 3)
+    All convolutions should be preceded by Batch Normalization and a
+    rectified linear activation (ReLU), respectively
+    All weights should use he normal initialization
+    The seed for the he_normal initializer should be set to zero
+    Returns: the keras model
+
+    """
