97af6d1058f19e749cbad79fa31777286f346bbc
LJThao <yengfong.thao@atlasschool.com>
2024-11-05 13:11:11 -0600

    func, doc

--- supervised_learning/deep_cnns/5-dense_block.py
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -3,3 +3,20 @@
 from tensorflow import keras as K
 
 
+def dense_block(X, nb_filters, growth_rate, layers):
+    """Function that builds a dense block as described in Densely
+    Connected Convolutional Networks:
+
+    X = the output from the previous layer
+    nb_filters = an integer representing the number of filters in X
+    growth_rate = the growth rate for the dense block
+    layers = the number of layers in the dense block
+    You should use the bottleneck layers used for DenseNet-B
+    All weights should use he normal initialization
+    The seed for the he_normal initializer should be set to zero
+    All convolutions should be preceded by Batch Normalization and a
+    rectified linear activation (ReLU), respectively
+    Returns: The concatenated output of each layer within the Dense Block
+    and the number of filters within the concatenated outputs, respectively
+
+    """
