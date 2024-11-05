bf6a47b15f1ea6e96f79040e285171d986e09185
LJThao <yengfong.thao@atlasschool.com>
2024-11-04 23:40:20 -0600

    func, import, docstr

--- supervised_learning/deep_cnns/1-inception_network.py
+++ supervised_learning/deep_cnns/1-inception_network.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
 """Inception Network Module"""
 from tensorflow import keras as K
+inception_block = __import__('0-inception_block').inception_block
 
 
+def inception_network():
+    """Function that builds the inception network as described in Going
+    Deeper with Convolutions (2014):
+
+    You can assume the input data will have shape (224, 224, 3)
+    All convolutions inside and outside the inception block should use a
+    rectified linear activation (ReLU)
+    You may use inception_block = __import__('0-inception_block').
+    inception_block
+    Returns: the keras model
+
+    """
