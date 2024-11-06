c6475a5ff629d5f63f2299b80421453e25835288
LJThao <yengfong.thao@atlasschool.com>
2024-11-05 22:28:28 -0600

    update func

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -20,14 +20,14 @@ def resnet50():
     """
     # init the HeNormal and input for shape
     init = K.initializers.HeNormal(seed=0)
-    inputs = K.Input(shape=(224, 224, 3))
+    input = K.Input(shape=(224, 224, 3))
 
     # init conv and pooling
     layer_output = K.layers.Conv2D(64, 7,
                                    strides=2,
                                    padding='same',
                                    kernel_initializer=init
-                                   )(inputs)
+                                   )(input)
     layer_output = K.layers.BatchNormalization()(layer_output)
     layer_output = K.layers.Activation('relu')(layer_output)
     layer_output = K.layers.MaxPooling2D(3,
@@ -35,15 +35,17 @@ def resnet50():
                                          padding='same'
                                          )(layer_output)
 
-    # the ResNet Stages, iterate over blocks
-    for filters, blocks, stride in [
+    # the ResNet Stages
+    stages = [
         ([64, 64, 256], 3, 1),
         ([128, 128, 512], 4, 2),
         ([256, 256, 1024], 6, 2),
         ([512, 512, 2048], 3, 2)
-    ]:
+    ]
+    # iterate over the stages
+    for filters, blocks, stride in stages:
         layer_output = projection_block(layer_output, filters, s=stride)
-        for _ in range(blocks - 1):
+        for _ in range(1, blocks):
             layer_output = identity_block(layer_output, filters)
 
     # average the pooling and connect the layer
@@ -51,7 +53,7 @@ def resnet50():
     output = K.layers.Dense(1000,
                             activation='softmax',
                             kernel_initializer=init)(layer_output)
-    model = K.Model(inputs, output)
+    keras_model = K.Model(inputs=input, outputs=layer_output)
 
     # returns the keras model 
-    return (model)
+    return (keras_model)
