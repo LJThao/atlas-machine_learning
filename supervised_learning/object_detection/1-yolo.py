7d8dc572e4c921d01b861f5b9da239c45e216495
LJThao <yengfong.thao@atlasschool.com>
2024-12-13 16:37:26 -0600

    test

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -79,29 +79,30 @@ class Yolo:
 
         for i, output in enumerate(outputs):
             grid_h, grid_w = output.shape[:2]
-            anchor_boxes = self.anchors[i].shape[0]
+            anchors = self.anchors[i]
 
             txy = output[..., :2]
             twh = output[..., 2:4]
+
             conf_sigmoid = self.sigmoid(output[..., 4:5])
             prob_sigmoid = self.sigmoid(output[..., 5:])
 
             conf_box = np.expand_dims(conf_sigmoid, axis=-1)
-
             box_confidences.append(conf_box)
             box_class_probs.append(prob_sigmoid)
 
-            box_wh = self.anchors[i] * np.exp(twh)
+            box_wh = anchors * np.exp(twh)
             box_wh /= [grid_w, grid_h]
 
-            grid = np.tile(np.indices((grid_w, grid_h)).T, anchor_boxes).reshape(grid_h, grid_w, -1, 2)
+            grid = np.indices((grid_w, grid_h)).T.reshape(grid_h, grid_w, 1, 2)
+            grid = np.tile(grid, (1, 1, anchors.shape[0], 1))
 
             box_xy = (self.sigmoid(txy) + grid) / [grid_w, grid_h]
+
             box_xy1 = box_xy - (box_wh / 2)
             box_xy2 = box_xy + (box_wh / 2)
 
             box = np.concatenate((box_xy1, box_xy2), axis=-1)
-
             box *= np.tile(image_size, 2)
 
             boxes.append(box)
