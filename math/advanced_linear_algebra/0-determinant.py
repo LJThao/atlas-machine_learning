df3204906fc21df40c3452ec47ee2e464528e4be
LJThao <yengfong.thao@atlasschool.com>
2025-01-15 19:27:19 -0600

    det fun + min

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Determinant Module"""
+"""Determinant Matrix Module"""
 
 
 def determinant(matrix):
@@ -14,4 +14,47 @@ def determinant(matrix):
     Returns: the determinant of matrix
 
     """
-    
+    # handles a 0x0 matrix
+    if matrix == [[]]:
+        return 1
+
+    # validating the input is a list of lists
+    if (
+        matrix and isinstance(matrix, list)
+        and all(isinstance(row, list) for row in matrix)
+    ):
+        # make sure the matrix is a square
+        size = len(matrix)
+        for row in matrix:
+            if len(row) != size:
+                raise ValueError("matrix must be a square matrix")
+
+        # 1x1 matrix
+        if size == 1:
+            return matrix[0][0]
+        # 2x2 matrix
+        elif size == 2:
+            return (
+                (matrix[0][0] * matrix[1][1]) -
+                (matrix[0][1] * matrix[1][0])
+            )
+        else:
+            # recursive for matrices
+            det_val = 0
+            for column_index in range(size):
+                # generates the minor matrix
+                minor = [
+                    [matrix[row][col] for col in range(size) if col != column_index]
+                    for row in range(1, size)
+                ]
+                # addition and subtraction for cofactors
+                cofact = matrix[0][column_index] * determinant(minor)
+                if column_index % 2 == 0:
+                    det_val += cofact
+                else:
+                    det_val-= cofact
+            return det_val
+
+    else:
+        # raise an error if the input is not valid
+        raise TypeError("matrix must be a list of lists")
