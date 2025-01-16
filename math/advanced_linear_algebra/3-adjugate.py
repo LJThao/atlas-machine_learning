f943481c46d7a3015379256dde656bdeee037908
LJThao <yengfong.thao@atlasschool.com>
2025-01-15 20:15:56 -0600

    docstr

--- math/advanced_linear_algebra/3-adjugate.py
+++ math/advanced_linear_algebra/3-adjugate.py
@@ -3,4 +3,15 @@
 
 
 def adjugate(matrix):
-    """"""
+    """
+
+    matrix is a list of lists whose adjugate matrix should be
+    calculated
+    If matrix is not a list of lists, raise a TypeError with
+    the message matrix must be a list of lists
+    If matrix is not square or is empty, raise a ValueError with
+    the message matrix must be a non-empty square matrix
+    Returns: the adjugate matrix of matrix
+
+    """
+    
