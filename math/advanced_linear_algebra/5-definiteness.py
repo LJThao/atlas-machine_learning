186f5b73ca2b9bded68ee684a3c74cb6a054757e
LJThao <yengfong.thao@atlasschool.com>
2025-01-15 21:37:09 -0600

    defin func

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -19,3 +19,28 @@ def definiteness(matrix):
     You may import numpy as np
 
     """
+    # validating matrix
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("matrix must be a numpy.ndarray")
+
+    # testing if the matrix is a square and symmetric
+    if matrix.ndim != 2 or matrix.shape[0] != matrix.shape[1]:
+        return None
+
+    # computing eigen values
+    eigen_val = np.linalg.eigvalsh(matrix)
+
+    # classifying definiteness
+    if np.all(eigen_val > 0):
+        return "Positive definite"
+    elif np.all(eigen_val >= 0):
+        return "Positive semi-definite"
+    elif np.all(eigen_val < 0):
+        return "Negative definite"
+    elif np.all(eigen_val <= 0):
+        return "Negative semi-definite"
+    elif np.any(eigen_val > 0) and np.any(eigen_val < 0):
+        return "Indefinite"
+
+    # returns none if matrix doesn't fit any categories
+    return None
