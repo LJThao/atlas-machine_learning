612d78b5e5b7a90edbd11102586dbb8b2426744a
LJThao <yengfong.thao@atlasschool.com>
2025-01-15 23:32:04 -0600

    likeli func

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -14,3 +14,21 @@ def likelihood(x, n, P):
     probabilities of developing severe side effects
 
     """
+    # validating all inputs
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError("x must be an integer that is greater than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if np.any((P < 0) | (P > 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    # computing the likelihood
+    fact = np.math.factorial
+    bino_coef = fact(n) / (fact(x) * fact(n - x))
+    likeli_val = bino_coef * (P ** x) * ((1 - P) ** (n - x))
+
+    return (likeli_val)
