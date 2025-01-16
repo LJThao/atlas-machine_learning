feffb1cff2c26dd416b22a71134a5dde17448b56
LJThao <yengfong.thao@atlasschool.com>
2025-01-15 20:26:37 -0600

    inv func, docstr

--- math/advanced_linear_algebra/4-inverse.py
+++ math/advanced_linear_algebra/4-inverse.py
@@ -2,3 +2,15 @@
 """Inverse Matrix Module"""
 
 
+def inverse(matrix):
+    """
+
+    matrix is a list of lists whose inverse should be calculated
+    If matrix is not a list of lists, raise a TypeError with the
+    message matrix must be a list of lists
+    If matrix is not square or is empty, raise a ValueError with
+    the message matrix must be a non-empty square matrix
+    Returns: the inverse of matrix, or None if matrix is singular
+
+    """
+    
