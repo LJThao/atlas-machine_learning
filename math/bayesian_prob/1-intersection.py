f6504bb55ccbce5fb6cfd81f6d0a867e97e4b1fe
LJThao <yengfong.thao@atlasschool.com>
2025-01-15 23:41:41 -0600

    inters func

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -14,3 +14,63 @@ def intersection(x, n, P, Pr):
     Pr is a 1D numpy.ndarray containing the prior beliefs of P
 
     """
+    # validating all inputs
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+        )
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    if np.any((P < 0) | (P > 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    if np.any((Pr < 0) | (Pr > 1)):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not np.isclose(Pr.sum(), 1):
+        raise ValueError("Pr must sum to 1")
+
+    # calculate the likelihood
+    lh = likelihood(x, n, P)
+
+    # calculate the intersection
+    inter = lh * Pr
+
+    return (inter)
+
+
+def likelihood(x, n, P):
+    """Function that calculates the likelihood of obtaining this
+    data given various hypothetical probabilities of developing
+    severe side effects:
+
+    x is the number of patients that develop severe side effects
+    n is the total number of patients observed
+    P is a 1D numpy.ndarray containing the various hypothetical
+    probabilities of developing severe side effects
+
+    """
+    # validating all inputs
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+            )
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if np.any((P < 0) | (P > 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    # computing the likelihood
+    fact = np.math.factorial
+    bino_coef = fact(n) / (fact(x) * fact(n - x))
+    likeli_val = bino_coef * (P ** x) * ((1 - P) ** (n - x))
+
+    return (likeli_val)
