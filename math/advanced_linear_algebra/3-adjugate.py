1ec5b0f216f535448d329120ebcba54b2461d1a4
LJThao <yengfong.thao@atlasschool.com>
2025-01-15 19:34:49 -0600

    func, docstr

--- math/advanced_linear_algebra/3-adjugate.py
+++ math/advanced_linear_algebra/3-adjugate.py
@@ -2,3 +2,13 @@
 """Minor Matrix Module"""
 
 
+def minor(matrix):
+    """
+
+    matrix is a list of lists whose minor matrix should be calculated
+    If matrix is not a list of lists, raise a TypeError with the message matrix must be a list of lists
+    If matrix is not square or is empty, raise a ValueError with the message matrix must be a non-empty square matrix
+    Returns: the minor matrix of matrix
+    
+    """
+    
