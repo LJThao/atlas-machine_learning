95266714e52706863b70eb76063ac9bc22e5a556
LJThao <yengfong.thao@atlasschool.com>
2025-01-23 18:01:34 -0600

    func, docstr

---
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""PCA V2 Module"""
+import numpy as np
+
+
+def pca(X, ndim):
+    """Function that performs PCA on a dataset:
+
+    X is a numpy.ndarray of shape (n, d) where:
+    n is the number of data points
+    d is the number of dimensions in each point
+    ndim is the new dimensionality of the transformed X
+    Returns: T, a numpy.ndarray of shape (n, ndim) containing
+    the transformed version of X
+
+    """
