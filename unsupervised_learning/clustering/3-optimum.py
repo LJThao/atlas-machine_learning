a4a275e7aa68120a6d8ad8b572baed390ed382e6
LJThao <yengfong.thao@atlasschool.com>
2025-01-27 23:59:50 -0600

    opt k

---
+++ unsupervised_learning/clustering/3-optimum.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Optimize k Module"""
+import numpy as np
+
+
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+    """Function that tests for the optimum number of clusters by
+    variance:
+
+    X is a numpy.ndarray of shape (n, d) containing the data set
+    kmin is a positive integer containing the minimum number of
+    clusters to check for (inclusive)
+    kmax is a positive integer containing the maximum number of
+    clusters to check for (inclusive)
+
+    Returns: results, d_vars, or None, None on failure
+    - results is a list containing the outputs of K-means
+    for each cluster size
+    - d_vars is a list containing the difference in variance
+    from the smallest cluster size for each cluster size
+
+    """
