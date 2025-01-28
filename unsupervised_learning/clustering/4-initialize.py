6dab8174ee27346754ae5708a04da7731382861f
LJThao <yengfong.thao@atlasschool.com>
2025-01-28 00:21:42 -0600

    pycode

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -4,7 +4,7 @@ import numpy as np
 kmeans = __import__('1-kmeans').kmeans
 
 
-def initialize(X, k): 
+def initialize(X, k):
     """Function that initializes variables for a Gaussian Mixture
     Model:
 
