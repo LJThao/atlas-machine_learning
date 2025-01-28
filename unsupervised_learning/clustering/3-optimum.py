71f2fc1c3bdeec1bbf42ab61082e59ff57738d71
LJThao <yengfong.thao@atlasschool.com>
2025-01-28 00:07:28 -0600

    import

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Optimize k Module"""
 import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
 
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
