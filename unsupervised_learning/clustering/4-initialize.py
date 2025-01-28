457c0cb23fb1d6d820873961ff9191304b12c219
LJThao <yengfong.thao@atlasschool.com>
2025-01-28 00:02:39 -0600

    gmm

---
+++ unsupervised_learning/clustering/4-initialize.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Initialize GMM Module"""
+import numpy as np
+
+
+def initialize(X, k): 
+    """Function that initializes variables for a Gaussian Mixture
+    Model:
+
+    X is a numpy.ndarray of shape (n, d) containing the data set
+    k is a positive integer containing the number of clusters
+    Returns: pi, m, S, or None, None, None on failure
+
+    """
