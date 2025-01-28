f149f57892130d5c839ae6f176a54d11a34868c9
LJThao <yengfong.thao@atlasschool.com>
2025-01-28 00:18:38 -0600

    aggl

---
+++ unsupervised_learning/clustering/12-agglomerative.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Agglomerative Module"""
+import scipy.cluster.hierarchy
+import matplotlib.pyplot as plt
+
+
+def agglomerative(X, dist):
+    """Function that performs agglomerative clustering on a dataset:
+
+    X is a numpy.ndarray of shape (n, d) containing the dataset
+    dist is the maximum cophenetic distance for all clusters
+    Returns: clss, a numpy.ndarray of shape (n,) containing the
+    cluster indices for each data point
+
+    """
