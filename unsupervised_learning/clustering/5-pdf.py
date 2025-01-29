8019e87c431936984a156182ac4fcbe2f37573c2
LJThao <yengfong.thao@atlasschool.com>
2025-01-29 16:12:57 -0600

    pdf func

--- unsupervised_learning/clustering/5-pdf.py
+++ unsupervised_learning/clustering/5-pdf.py
@@ -16,3 +16,26 @@ def pdf(X, m, S):
     Returns: P, or None on failure
 
     """
+    if (not isinstance(X, np.ndarray) or X.ndim != 2 or
+            not isinstance(m, np.ndarray) or m.ndim != 1 or
+            not isinstance(S, np.ndarray) or S.ndim != 2 or
+            X.shape[1] != m.shape[0] or m.shape[0] != S.shape[0] or
+            S.shape[0] != S.shape[1]):
+        return None
+
+    d = X.shape[1]
+    
+    try:
+        det_S = np.linalg.det(S)
+        inv_S = np.linalg.inv(S)
+    except np.linalg.LinAlgError:
+        return None
+
+    n = 1 / (np.sqrt((2 * np.pi) ** d * det_S))
+
+    diff = X - m
+    exponent = -0.5 * np.sum(diff @ inv_S * diff, axis=1)
+
+    P = n * np.exp(exponent)
+
+    return P
