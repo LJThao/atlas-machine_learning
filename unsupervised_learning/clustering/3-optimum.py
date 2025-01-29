e9e4be707f6a2c5dd693ad0bb0b6185138cb41e2
LJThao <yengfong.thao@atlasschool.com>
2025-01-28 23:45:28 -0600

    opt func

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -22,3 +22,32 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     from the smallest cluster size for each cluster size
 
     """
+    # validating all the input parameters
+    if (not isinstance(X, np.ndarray) or X.ndim != 2 or
+            not isinstance(kmin, int) or kmin <= 0 or
+            (kmax is not None and (not isinstance(kmax, int) or kmax < kmin)) or
+            not isinstance(iterations, int) or iterations <= 0):
+        return None, None
+
+    # setting kmax value
+    kmax = kmax or X.shape[0]
+
+    results, d_vars = [], []
+    base_var = None
+
+    # iterating over the range
+    for k in range(kmin, kmax + 1):
+        centroids, labels = kmeans(X, k, iterations)
+        if centroids is None:
+            return None, None
+
+        c_var = variance(X, centroids)
+        results.append((centroids, labels))
+
+        if base_var is None:
+            base_var = c_var
+            d_vars.append(0.0)
+        else:
+            d_vars.append(abs(base_var - c_var))
+
+    return results, d_vars
