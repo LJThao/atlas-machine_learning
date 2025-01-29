d9388c1ccc67a2863e20617063ff4de6eac8cb9a
LJThao <yengfong.thao@atlasschool.com>
2025-01-28 23:53:19 -0600

    init func

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -13,3 +13,22 @@ def initialize(X, k):
     Returns: pi, m, S, or None, None, None on failure
 
     """
+    # validating all inputs parameters
+    if (not isinstance(X, np.ndarray) or X.ndim != 2 or
+            not isinstance(k, int) or k <= 0 or k > X.shape[0]):
+        return None, None, None
+
+    d = X.shape[1]
+
+    # init means using k-means
+    m, _ = kmeans(X, k)
+    if m is None:
+        return None, None, None
+
+    # init covariance matrices
+    S = np.eye(d)[None, :, :] * np.ones((k, 1, 1))
+
+    # init priors equally for all clusters
+    pi = np.full(k, 1 / k)
+
+    return pi, m, S
