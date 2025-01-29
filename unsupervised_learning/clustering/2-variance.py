3f825c20aec180b2af9a5044ca244085f901ce8f
LJThao <yengfong.thao@atlasschool.com>
2025-01-28 23:32:13 -0600

    var func

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -10,6 +10,18 @@ def variance(X, C):
     X is a numpy.ndarray of shape (n, d) containing the data set
     C is a numpy.ndarray of shape (k, d) containing the centroid
     means for each cluster
-    Returns: var, or None on failure, var is the total variance
+    Returns: total variance, or None on failure
 
     """
+    if not isinstance(X, np.ndarray) or not isinstance(C, np.ndarray):
+        return None
+    if X.ndim != 2 or C.ndim != 2:
+        return None
+    if X.shape[1] != C.shape[1]:
+        return None
+
+    centroids = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
+
+    total_variance = np.sum(np.min(centroids, axis=1) ** 2)
+
+    return total_variance
