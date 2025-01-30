895c159c0b23c442cedfff20dfe5eaf32daca169
LJThao <yengfong.thao@atlasschool.com>
2025-01-29 19:11:50 -0600

    pycode

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -19,13 +19,13 @@ def expectation(X, pi, m, S):
 
     """
     if (not isinstance(X, np.ndarray) or X.ndim != 2 or
-        not isinstance(pi, np.ndarray) or pi.ndim != 1 or
-        not isinstance(m, np.ndarray) or m.ndim != 2 or
-        not isinstance(S, np.ndarray) or S.ndim != 3 or
-        X.shape[1] != m.shape[1] or
-        m.shape[0] != pi.shape[0] or S.shape[0] != pi.shape[0] or
-        S.shape[1] != S.shape[2] or S.shape[1] != X.shape[1] or
-        not np.isclose(np.sum(pi), 1)):
+            not isinstance(pi, np.ndarray) or pi.ndim != 1 or
+            not isinstance(m, np.ndarray) or m.ndim != 2 or
+            not isinstance(S, np.ndarray) or S.ndim != 3 or
+            X.shape[1] != m.shape[1] or
+            m.shape[0] != pi.shape[0] or S.shape[0] != pi.shape[0] or
+            S.shape[1] != S.shape[2] or S.shape[1] != X.shape[1] or
+            not np.isclose(np.sum(pi), 1)):
         return None, None
 
     k = pi.shape[0]
