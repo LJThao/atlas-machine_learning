b76e34e41c4ea35ba1863ec405485e95599f1058
LJThao <yengfong.thao@atlasschool.com>
2025-01-29 23:35:35 -0600

    gmm func

--- unsupervised_learning/clustering/11-gmm.py
+++ unsupervised_learning/clustering/11-gmm.py
@@ -8,6 +8,18 @@ def gmm(X, k):
 
     X is a numpy.ndarray of shape (n, d) containing the dataset
     k is the number of clusters
+    pi is priors for each cluster
+    m is centroid means for each cluster
+    S is covariance matrices for each cluster
+    clss contain the cluster indices for each data point
+    bic contain the BIC value for each cluster size tested
     Returns: pi, m, S, clss, bic
 
     """
+    model = sklearn.mixture.GaussianMixture(k)
+    model.fit(X)
+
+    pi, m, S = model.weights_, model.means_, model.covariances_
+    clss, bic = model.predict(X), model.bic(X)
+
+    return pi, m, S, clss, bic
