ecf038d329c9950873ecacbfd0441640d5a353eb
LJThao <yengfong.thao@atlasschool.com>
2025-01-29 21:10:07 -0600

    exp max func

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -8,11 +8,51 @@ maximization = __import__('7-maximization').maximization
 
 def expectation_maximization(X, k, iterations=1000,
                              tol=1e-5, verbose=False):
-    """Functionthat performs the expectation maximization for a GMM:
+    """Function that perform the expectation maximization for a GMM:
 
     X is a numpy.ndarray of shape (n, d) containing the data set
     k is a positive integer containing the number of clusters
+    pi is priors for each cluster
+    m is centroid means for each cluster
+    S is covariance matrices for each cluster
+    g is probabilities for each data point in each cluster
+    log_l is log likelihood of the model
     Returns: pi, m, S, g, l, or None, None, None, None, None on
     failure
 
     """
+    # validate all input
+    if (not isinstance(X, np.ndarray) or X.ndim != 2 or
+            not isinstance(k, int) or k <= 0 or
+            not isinstance(iterations, int) or iterations <= 0 or
+            not isinstance(tol, float) or tol < 0 or
+            not isinstance(verbose, bool)):
+        return None, None, None, None, None
+
+    # init all parameters
+    pi, m, S = initialize(X, k)
+    g, log_l = expectation(X, pi, m, S)
+    
+    if g is None:
+        return None, None, None, None, None
+
+    # iterate range, update parameters, compute probabilities
+    for i in range(iterations):
+        pi, m, S = maximization(X, g)
+        g, new_log_l = expectation(X, pi, m, S)
+        
+        if g is None:
+            return None, None, None, None, None
+
+        # print the log if verbose every 10
+        if verbose and (i % 10 == 0 or i == iterations - 1):
+            print(f"Log Likelihood after {i} iterations: {new_log_l:.5f}")
+
+        # if log stops improving, loop breaks
+        if abs(new_log_l - log_l) <= tol:
+            break
+
+        # update likelihood
+        log_l = new_log_l
+
+    return pi, m, S, g, log_l
