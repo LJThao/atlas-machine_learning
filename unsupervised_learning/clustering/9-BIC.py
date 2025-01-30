6118c1c91555600b7a91f644fdd8ee98545032c4
LJThao <yengfong.thao@atlasschool.com>
2025-01-29 23:07:36 -0600

    bic func

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -14,7 +14,62 @@ def BIC(X, kmin=1, kmax=None, iterations=1000,
     clusters to check for (inclusive)
     kmax is a positive integer containing the maximum number of
     clusters to check for (inclusive)
+    pi is priors for each cluster
+    m is centroid means for each cluster
+    S is covariance matrices for each cluster
+    l is log likelihood for each cluster size tested
+    b is the BIC value for each cluster size tested
+    p is the number of parameters
+    n is the number of data points
+    best_k is the best value for k based on its BIC
+    best_result is the tuple containing pi, m, S
     Returns: best_k, best_result, l, b, or None, None, None, None
     on failure
 
     """
+    # validate all parameters
+    if (not isinstance(X, np.ndarray) or X.ndim != 2 or
+            not isinstance(kmin, int) or kmin < 1 or
+            (kmax is not None and (not isinstance(kmax, int) or
+                                   kmax < kmin)) or
+            not isinstance(iterations, int) or iterations <= 0 or
+            not isinstance(tol, float) or tol < 0 or
+            not isinstance(verbose, bool)):
+        return None, None, None, None
+
+    n, d = X.shape
+
+    # init lists
+    best_k_values = []
+    best_results = []
+    l = []
+    b = []
+
+    # run expectation max for each k in range
+    for k in range(kmin, kmax + 1):
+        pi, m, S, _, log_l = expectation_maximization(X, k,
+                                                      iterations,
+                                                      tol, verbose)
+
+        # store values for each k
+        best_k_values.append(k)
+        best_results.append((pi, m, S))
+        l.append(log_l)
+
+        # compute parameters p
+        p = (k * d) + (k * d * (d + 1) / 2) + (k - 1)
+
+        # compute BIC
+        bic = p * np.log(n) - (2 * log_l)
+        b.append(bic)
+
+    # convert lists to arrays
+    b = np.array(b)
+    l = np.array(l)
+
+    # finding best k
+    best_idx = np.argmin(b)
+    best_k = best_k_values[best_idx]
+    best_result = best_results[best_idx]
+
+    return best_k, best_result, l, b
