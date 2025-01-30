659cec1f8f5cb3ea155f477bab787762a2d31e0e
LJThao <yengfong.thao@atlasschool.com>
2025-01-29 23:27:31 -0600

    kmean func

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -9,5 +9,14 @@ def kmeans(X, k):
     X is a numpy.ndarray of shape (n, d) containing the dataset
     k is the number of clusters
     Returns: C, clss
+    - C contains centroid means for each cluster
+    - clss contains the index of the cluster in C
 
     """
+    # init and and fit the k-means model
+    model = sklearn.cluster.KMeans(n_clusters=k, n_init='auto').fit(X)
+
+    # extracting the clusters
+    C, clss = model.cluster_centers_, model.labels_
+
+    return C, clss
