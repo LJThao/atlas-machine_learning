032c2f24e558bcf0817f78f3bc354ce3c10294ed
LJThao <yengfong.thao@atlasschool.com>
2025-02-03 22:59:36 -0600

    absorbing func

---
+++ unsupervised_learning/hmm/2-absorbing.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Absorbing Chains Module"""
+import numpy as np
+
+
+def absorbing(P):
+    """Function that determines if a markov chain is absorbing:
+
+    P is a is a square 2D numpy.ndarray of shape (n, n)
+    representing the standard transition matrix
+    P[i, j] is the probability of transitioning from state i
+    to state j
+    n is the number of states in the markov chain
+    Returns: True if it is absorbing, or False on failure
+
+    """
