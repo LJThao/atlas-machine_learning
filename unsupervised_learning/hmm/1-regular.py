cc07fa413dd99f30975c44d4e4c26874e402076c
LJThao <yengfong.thao@atlasschool.com>
2025-02-04 11:34:07 -0600

    regular func

--- unsupervised_learning/hmm/1-regular.py
+++ unsupervised_learning/hmm/1-regular.py
@@ -16,3 +16,15 @@ def regular(P):
     state probabilities, or None on failure
 
     """
+    if (not isinstance(P, np.ndarray) or P.ndim != 2 or 
+            P.shape[0] != P.shape[1]):
+        return None
+    
+    if np.any(P <= 0) or np.any(P >= 1):
+        return None
+
+    i = 100
+    trans_mat = np.linalg.matrix_power(P, i)
+    state_prob = trans_mat[0, :]
+
+    return state_prob[np.newaxis, :]
