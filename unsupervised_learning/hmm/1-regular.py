ca040b10d24e4023dc48844d6d0a0239dfb33887
LJThao <yengfong.thao@atlasschool.com>
2025-02-03 23:09:45 -0600

    pycode

--- unsupervised_learning/hmm/1-regular.py
+++ unsupervised_learning/hmm/1-regular.py
@@ -9,8 +9,8 @@ def regular(P):
 
     P is a is a square 2D numpy.ndarray of shape (n, n) representing
     the transition matrix
-    P[i, j] is the probability of transitioning from state i to state
-    j
+    P[i, j] is the probability of transitioning from state i to
+    state j
     n is the number of states in the markov chain
     Returns: a numpy.ndarray of shape (1, n) containing the steady
     state probabilities, or None on failure  
