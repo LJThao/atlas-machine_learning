6fe87b50ad703be1826088dca2b11cebeab19af9
LJThao <yengfong.thao@atlasschool.com>
2025-02-17 15:28:44 -0600

    pred func

--- unsupervised_learning/hyperparameter_tuning/1-gp.py
+++ unsupervised_learning/hyperparameter_tuning/1-gp.py
@@ -34,3 +34,33 @@ class GaussianProcess():
             - 2 * np.dot(X1, X2.T)
         )
         return self.sigma_f**2 * np.exp(-0.5 * sqdist / self.l**2)
+
+    def predict(self, X_s):
+        """Function that predicts the mean and standard deviation of points
+        in a Gaussian process:
+
+        X_s is a numpy.ndarray of shape (s, 1) containing all of the points
+        whose mean and standard deviation should be calculated
+         -> s is the number of sample points
+
+        Returns: mu, sigma
+        mu is a numpy.ndarray of shape (s,) containing the mean for each
+        point in X_s, respectively
+        sigma is a numpy.ndarray of shape (s,) containing the variance
+        for each point in X_s, respectively
+
+        """
+        # calculate covariance
+        K_s = self.kernel(self.X, X_s)
+        K_ss = self.kernel(X_s, X_s)
+        # calculate inverses of the covariance mat
+        K_inv = np.linalg.inv(self.K)
+
+        # calculate the mean of predictive dist
+        mu = K_s.T @ K_inv @ self.Y
+
+        # calculate the variance of predictive dist
+        sigma = np.diag(K_ss - K_s.T @ K_inv @ K_s)
+        
+        # return the mu and sigma
+        return mu.flatten(), sigma
