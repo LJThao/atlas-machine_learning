755346dada4aacf0db948833ca8f80dc6210b787
LJThao <yengfong.thao@atlasschool.com>
2025-03-04 01:35:28 -0600

    sparse func

--- unsupervised_learning/autoencoders/1-sparse.py
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -20,3 +20,31 @@ def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
     auto is the sparse autoencoder model
 
     """
+    # build the encoder - compressing the inputs
+    encode_input = keras.Input(shape=(input_dims,))
+    x = encode_input
+    for units in hidden_layers:
+        x = keras.layers.Dense(units, activation="relu")(x)
+
+    # add L1 regularization to the latent layer
+    x = keras.layers.Dense(
+        latent_dims, activation="relu",
+        activity_regularizer=keras.regularizers.l1(lambtha)
+    )(x)
+    encoder = keras.Model(encode_input, x, name="encoder")
+
+    # build the decoder - reconstructing the original input
+    decode_input = keras.Input(shape=(latent_dims,))
+    x = decode_input
+    for units in reversed(hidden_layers):
+        x = keras.layers.Dense(units, activation="relu")(x)
+    x = keras.layers.Dense(input_dims, activation="sigmoid")(x)
+    decoder = keras.Model(decode_input, x, name="decoder")
+
+    # combine encoder + decoder to make the sparse autoencoder
+    auto = keras.Model(
+        encode_input, decoder(encoder(encode_input)), name="autoencoder"
+    )
+    auto.compile(optimizer="adam", loss="binary_crossentropy")
+
+    return encoder, decoder, auto
