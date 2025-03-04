a398d0f6512124dc037c8b1cc72d289ae4ddc62c
LJThao <yengfong.thao@atlasschool.com>
2025-03-04 12:16:48 -0600

    var func

--- unsupervised_learning/autoencoders/3-variational.py
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -19,3 +19,54 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     auto is the full autoencoder model
 
     """
+    def sample_z(args):
+        """Function that transforms a std normal random var into a sample"""
+        mean, log_var = args
+        epsilon = keras.backend.random_normal(shape=keras.backend.shape(mean))
+        return mean + keras.backend.exp(0.5 * log_var) * epsilon
+
+    inputs = keras.Input(shape=(input_dims,))
+    x = inputs
+    for units in hidden_layers:
+        x = keras.layers.Dense(units, activation="relu")(x)
+
+    mean = keras.layers.Dense(latent_dims, activation=None)(x)
+    log_var = keras.layers.Dense(latent_dims, activation=None)(x)
+
+    z = keras.layers.Lambda(sample_z)([mean, log_var])
+
+    encoder = keras.Model(inputs, [z, mean, log_var], name="encoder")
+
+    latent_inputs = keras.Input(shape=(latent_dims,))
+    x = latent_inputs
+    for units in reversed(hidden_layers):
+        x = keras.layers.Dense(units, activation="relu")(x)
+
+    outputs = keras.layers.Dense(input_dims, activation="sigmoid")(x)
+
+    decoder = keras.Model(latent_inputs, outputs, name="decoder")
+
+    auto_outputs = decoder(encoder(inputs)[0])
+    auto = keras.Model(inputs, auto_outputs, name="autoencoder")
+
+    def variational_loss(y_true, y_pred):
+        """Function that computes the VAE loss as the sum of reconstruction
+        loss and KL divergence. Ensures the latent space follows a std
+        normal distribution while minimizing reconstruction error."""
+        z_mean, z_log_var = y_pred[1], y_pred[2]
+
+        rec_loss = keras.losses.binary_crossentropy(y_true, y_pred[0])
+        rec_loss = keras.backend.sum(rec_loss, axis=-1)
+
+        kl_loss = -0.5 * keras.backend.sum(
+            1 + z_log_var
+            - keras.backend.square(z_mean)
+            - keras.backend.exp(z_log_var),
+            axis=-1,
+        )
+
+        return keras.backend.mean(rec_loss + kl_loss)
+
+    auto.compile(optimizer="adam", loss=variational_loss)
+
+    return encoder, decoder, auto
