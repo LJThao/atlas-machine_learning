c3966ec8dcd7c8db71e7b8a598a9ca866d6672ab
LJThao <yengfong.thao@atlasschool.com>
2025-03-04 00:38:26 -0600

    pycode

--- unsupervised_learning/autoencoders/2-convolutional.py
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -11,16 +11,18 @@ def autoencoder(input_dims, filters, latent_dims):
     filters is a list containing the number of filters for each convolutional
     layer in the encoder, respectively
     the filters should be reversed for the decoder
-    latent_dims is a tuple of integers containing the dimensions of the latent
-    space representation
-    Each convolution in the encoder should use a kernel size of (3, 3) with same
-    padding and relu activation, followed by max pooling of size (2, 2)
-    Each convolution in the decoder, except for the last two, should use a filter
-    size of (3, 3) with same padding and relu activation, followed by upsampling of
+    latent_dims is a tuple of integers containing the dimensions of
+    the latent space representation
+    Each convolution in the encoder should use a kernel size of (3, 3)
+    with same padding and relu activation, followed by max pooling of
     size (2, 2)
+    Each convolution in the decoder, except for the last two,
+    should use a filter size of (3, 3) with same padding and relu activation,
+    followed by
+    upsampling of size (2, 2)
     The second to last convolution should instead use valid padding
-    The last convolution should have the same number of filters as the number of
-    channels in input_dims with sigmoid activation and no upsampling
+    The last convolution should have the same number of filters as the
+    number of channels in input_dims with sigmoid activation and no upsampling
     Returns: encoder, decoder, auto
     encoder is the encoder model
     decoder is the decoder model
