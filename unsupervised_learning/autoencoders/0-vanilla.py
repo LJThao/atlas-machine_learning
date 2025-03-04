ae61431cf173088fb05873b0c34bd4472d859dd0
LJThao <yengfong.thao@atlasschool.com>
2025-03-04 00:18:25 -0600

    autoencoder func

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -18,3 +18,26 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     auto is the full autoencoder model
 
     """
+    # build the encoder - compressing the inputs
+    inputs = keras.Input(shape=(input_dims,))
+    x = inputs
+    for units in hidden_layers:
+        x = keras.layers.Dense(units, activation='relu')(x)
+    x = keras.layers.Dense(latent_dims, activation='relu')(x)
+    encoder = keras.Model(inputs, x, name='encoder')
+
+    # build the decoder - reconstructing the original input
+    inputs = keras.Input(shape=(latent_dims,))
+    x = inputs
+    for units in reversed(hidden_layers):
+        x = keras.layers.Dense(units, activation='relu')(x)
+    x = keras.layers.Dense(input_dims, activation='sigmoid')(x)
+    decoder = keras.Model(inputs, x, name='decoder')
+
+    # combining the encoder + decoder to make the autoencoder
+    auto = keras.Model(encoder.input,
+                       decoder(encoder.output),
+                       name='autoencoder')
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
