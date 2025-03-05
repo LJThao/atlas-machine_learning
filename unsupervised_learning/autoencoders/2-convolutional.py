62dd86d89c11df639230219bc51ce9df94dcc6d5
LJThao <yengfong.thao@atlasschool.com>
2025-03-05 14:40:06 -0600

    test

--- unsupervised_learning/autoencoders/2-convolutional.py
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -46,11 +46,13 @@ def autoencoder(input_dims, filters, latent_dims):
         x = keras.layers.Conv2D(
             f, kernel_size=(3, 3), padding="same", activation="relu"
         )(x)
-    x = keras.layers.UpSampling2D(size=(2, 2))(x)
+        x = keras.layers.UpSampling2D(size=(2, 2))(x)
 
     x = keras.layers.Conv2D(
         filters[0], kernel_size=(3, 3), padding="valid", activation="relu"
     )(x)
+
+    x = keras.layers.UpSampling2D(size=(2, 2))(x)
 
     x = keras.layers.Conv2D(
         input_dims[-1],
