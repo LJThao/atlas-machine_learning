f2394548b7f69026966ced526af61d66b7f6c6c4
LJThao <yengfong.thao@atlasschool.com>
2025-03-19 23:11:24 -0500

    bidir

---
+++ supervised_learning/RNNs/5-bi_forward.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Bidirectional Cell Forward Module"""
+import numpy as np
+
+
+class BidirectionalCell():
+    """Class that represents a bidirectional cell of an RNN:"""
+    def __init__(self, i, h, o):
+        """"
+
+        i is the dimensionality of the data
+        h is the dimensionality of the hidden states
+        o is the dimensionality of the outputs
+
+        """
