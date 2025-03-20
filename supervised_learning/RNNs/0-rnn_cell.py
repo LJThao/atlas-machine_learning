231cadaf4dc1c688bd390de1dbbde8cad6ca92b8
LJThao <yengfong.thao@atlasschool.com>
2025-03-19 22:56:56 -0500

    class

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -3,3 +3,13 @@
 import numpy as np
 
 
+class RNNCell():
+    """Class that represents a cell of a simple RNN"""
+    def __init__(self, i, h, o):
+        """
+
+        i is the dimensionality of the data
+        h is the dimensionality of the hidden state
+        o is the dimensionality of the outputs
+
+        """
