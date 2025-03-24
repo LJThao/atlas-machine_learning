c28dc6b597f9970e3272204ddf5caefab5ce1ed8
LJThao <yengfong.thao@atlasschool.com>
2025-03-24 10:32:54 -0500

    rnn cell

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -13,3 +13,25 @@ class RNNCell():
         o is the dimensionality of the outputs
 
         """
+        # init the weights and biases
+        self.Wh = np.random.randn(i + h, h)
+        self.bh = np.zeros((1, h))
+        self.Wy = np.random.randn(h, o)
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """
+
+        x_t is a numpy.ndarray of shape (m, i) that contains
+        the data input for the cell
+        m is the batche size for the data
+        h_prev is a numpy.ndarray of shape (m, h) containing
+        the previous hidden state
+
+        """
+        # concatenate, compute using tanh and softmax
+        h_x = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(h_x @ self.Wh + self.bh)
+        y = np.exp(h_next @ self.Wy + self.by)
+
+        return h_next, y / y.sum(axis=1, keepdims=True)
