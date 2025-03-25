dbd860310821d293dfde1b9d94b0ba0024e16824
LJThao <yengfong.thao@atlasschool.com>
2025-03-24 20:58:01 -0500

    biout

--- supervised_learning/RNNs/7-bi_output.py
+++ supervised_learning/RNNs/7-bi_output.py
@@ -14,3 +14,58 @@ class BidirectionalCell():
         o is the dimensionality of the outputs
 
         """
+        # init weights and biases
+        self.Whf = np.random.randn(i + h, h)
+        self.bhf = np.zeros((1, h))
+        self.Whb = np.random.randn(i + h, h)
+        self.bhb = np.zeros((1, h))
+        self.Wy = np.random.randn(2 * h, o)
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """Function that calculates the hidden state in the
+        forward direction for one tim:
+
+        x_t is a numpy.ndarray of shape (m, i) that contains
+        the data input for the cell
+        m is the batch size for the data
+        h_prev is a numpy.ndarray of shape (m, h) containing
+        the previous hidden state
+
+        """
+        h_x = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(h_x @ self.Whf + self.bhf)
+
+        return h_next
+
+    def backward(self, h_next, x_t):
+        """Function that calculates the hidden state in the
+        backward direction for one time step:
+
+        x_t is a numpy.ndarray of shape (m, i) that contains the data input
+        for the cell
+        m is the batch size for the data
+        h_next is a numpy.ndarray of shape (m, h) containing the next hidden
+        state
+
+        """
+        h_x = np.concatenate((h_next, x_t), axis=1)
+        h_prev = np.tanh(h_x @ self.Whb + self.bhb)
+
+        return h_prev
+
+    def output(self, H):
+        """Function that calculates all outputs for the RNN:
+
+        H is a numpy.ndarray of shape (t, m, 2 * h) that contains the concatenated
+        hidden states from both directions, excluding their initialized states
+        t is the number of time steps
+        m is the batch size for the data
+        h is the dimensionality of the hidden states
+
+        """
+        t, m, _ = H.shape
+        Y = np.exp(H @ self.Wy + self.by)
+        Y = Y / Y.sum(axis=2, keepdims=True)
+
+        return Y
