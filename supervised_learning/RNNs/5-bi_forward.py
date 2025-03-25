1ac2f180cdf9d4b33f70b7900ed1865e74bfd884
LJThao <yengfong.thao@atlasschool.com>
2025-03-24 20:03:56 -0500

    bifor

--- supervised_learning/RNNs/5-bi_forward.py
+++ supervised_learning/RNNs/5-bi_forward.py
@@ -13,3 +13,26 @@ class BidirectionalCell():
         o is the dimensionality of the outputs
 
         """
+        # init weights and biases
+        self.Whf = np.random.randn(i + h, h)
+        self.bhf = np.zeros((1, h))
+        self.Whb = np.random.randn(i + h, h)
+        self.bhb = np.zeros((1, h))
+        self.Wy = np.random.randn(2 * h, o)
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """Function that calculates the hidden state in the
+        forward direction for one tim:
+
+        x_t is a numpy.ndarray of shape (m, i) that contains
+        the data input for the cell
+        m is the batch size for the data
+        h_prev is a numpy.ndarray of shape (m, h) containing
+        the previous hidden state
+
+        """
+        h_x = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(h_x @ self.Whf + self.bhf)
+
+        return h_next
