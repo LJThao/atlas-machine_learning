0ba2e543851f89fd8986fd9e7e0c49491035e99d
LJThao <yengfong.thao@atlasschool.com>
2025-04-07 18:49:53 -0500

    func

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 """TF-IDF Module"""
+
+
+def tf_idf(sentences, vocab=None):
+    """Function that creates a TF-IDF embedding:
+
+    sentences is a list of sentences to analyze
+    vocab is a list of the vocabulary words to use for the analysis
+    If None, all words within sentences should be used
+    Returns: embeddings, features
+
+    """
