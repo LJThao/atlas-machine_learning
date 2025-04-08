79ccc4c1fdea8c6bc7332105d60dc775046acdb8
LJThao <yengfong.thao@atlasschool.com>
2025-04-07 21:58:13 -0500

    extract func

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -11,3 +11,15 @@ def gensim_to_keras(model):
     Returns: the trainable keras Embedding
 
     """
+    # gets words from the model
+    weights = model.wv.vectors
+
+    # creates keras embedding layer
+    trainable_embedding = tf.keras.layers.Embedding(
+        input_dim=weights.shape[0],
+        output_dim=weights.shape[1],
+        weights=[weights],
+        trainable=True
+    )
+
+    return trainable_embedding
