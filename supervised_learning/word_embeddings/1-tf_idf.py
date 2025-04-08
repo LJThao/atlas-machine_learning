449080ca1c7afc5f9ce79f0579026988dde35f6b
LJThao <yengfong.thao@atlasschool.com>
2025-04-07 21:23:30 -0500

    tf func

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """TF-IDF Module"""
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def tf_idf(sentences, vocab=None):
@@ -11,3 +12,14 @@ def tf_idf(sentences, vocab=None):
     Returns: embeddings, features
 
     """
+    # create the vectorizer, using the provided vocab
+    v = TfidfVectorizer(vocabulary=vocab)
+
+    # learn the vocabulary and compute the TF-IDF matrix
+    embeddings = v.fit_transform(sentences).toarray()
+
+    # get the list of features (words used)
+    features = v.get_feature_names_out()
+
+    # return the TF-IDF matrix and feature list
+    return embeddings, features
