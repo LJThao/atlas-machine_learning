2e05c227fc90591176f1f376d002b2e7992c5418
LJThao <yengfong.thao@atlasschool.com>
2025-05-13 17:06:32 -0500

    frozenlakeenv func

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -16,3 +16,11 @@ def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     Returns: the environment
 
     """
+    frozen_env = gym.make(
+        "FrozenLake-v1",
+        desc=desc,
+        map_name=map_name,
+        is_slippery=is_slippery
+    )
+
+    return frozen_env
