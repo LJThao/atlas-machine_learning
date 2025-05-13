f90fec44a77e2f3e46665c25654f5e7cc262d96c
LJThao <yengfong.thao@atlasschool.com>
2025-05-13 17:08:21 -0500

    init qtable module

---
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Initialize Q-table Module"""
+import numpy as np
+
+
+def q_init(env):
+    """Function that initializes the Q-table:
+
+    env is the FrozenLakeEnv instance
+    Returns: the Q-table as a numpy.ndarray of zeros
+
+    """
