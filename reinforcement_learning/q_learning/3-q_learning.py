1494fd212ebbaadefd233d3ef707ca4deb29b20e
LJThao <yengfong.thao@atlasschool.com>
2025-05-13 19:43:11 -0500

    q-learning func

--- reinforcement_learning/q_learning/3-q_learning.py
+++ reinforcement_learning/q_learning/3-q_learning.py
@@ -21,3 +21,37 @@ def train(env, Q, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99,
     Returns: Q, total_rewards
 
     """
+    # store the rewards
+    total_rewards = []
+
+    for episode in range(episodes):
+        state, _ = env.reset()
+        episode_reward = 0
+
+        for _ in range(max_steps):
+            # pick and do acti
+            action = epsilon_greedy(Q, state, epsilon)
+            new_state, reward, done, _, _ = env.step(action)
+
+            # applying hole penalty
+            if done and reward == 0:
+                reward = -1
+
+            # updating Q
+            Q[state, action] = (1 - alpha) * Q[state, action] + \
+                alpha * (reward + gamma * Q[new_state].max())
+
+            state = new_state
+            episode_reward += reward
+
+            if done == True:
+                break
+
+        # lowering epsilon
+        epsilon = min_epsilon + (1 - min_epsilon) * \
+            np.exp(-epsilon_decay * episode)
+
+        # saving the reward
+        total_rewards.append(episode_reward)
+
+    return Q, total_rewards
