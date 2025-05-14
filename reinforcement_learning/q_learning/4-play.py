d113ae9f62c9731f2ab9a4ee8d1fe17ba467f12a
LJThao <yengfong.thao@atlasschool.com>
2025-05-13 20:30:06 -0500

    play func and updated load-env

--- reinforcement_learning/q_learning/4-play.py
+++ reinforcement_learning/q_learning/4-play.py
@@ -11,3 +11,28 @@ def play(env, Q, max_steps=100):
     max_steps is the maximum number of steps in the episode
 
     """
+    # store the board
+    rendered_outputs = []
+
+    # starting the game
+    state, _ = env.reset()
+    total_reward = 0
+
+    for _ in range(max_steps):
+        # save the board
+        rendered_outputs.append(env.render())
+
+        # choose the best action, do the action
+        action = np.argmax(Q[state])
+        state, reward, done, _, _ = env.step(action)
+        total_reward += reward
+
+        # stop the game if it is over
+        if done == True:
+            break
+
+    # save the final board
+    rendered_outputs.append(env.render())
+
+    # return the score and steps
+    return total_reward, rendered_outputs
