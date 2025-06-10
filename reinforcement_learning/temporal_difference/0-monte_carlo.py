d2969f911743b76a07ec1f64b7c8eeb8b80aebe4
LJThao <yengfong.thao@atlasschool.com>
2025-06-09 20:04:17 -0500

    func

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -18,3 +18,45 @@ def monte_carlo(env, V, policy, episodes=5000,
     Returns: V, the updated value estimate
 
     """
+    # looping over the episodes, then store states and rewards
+    for episode in range(episodes):
+        states = []
+        rewards = []
+
+        # starting the episode
+        state, _ = env.reset()
+
+        # generate an episode, following the policy
+        for step in range(max_steps):
+            action = policy(state)
+            next_state, reward, terminated, truncated, _ = env.step(action)
+
+            states.append(state)
+            rewards.append(reward)
+
+            if terminated or truncated:
+                break
+
+            # move to the next state
+            state = next_state
+
+        # init return
+        G = 0
+
+        # processing the episode backward to calculate returns
+        for t in range(len(states) - 1, -1, -1):
+            G = rewards[t] + gamma * G
+            s = states[t]
+
+            # determining earlier states
+            if episode < len(states):
+                seen_states = states[:episode]
+            else:
+                seen_states = states[:len(states)]
+
+            # only update the state not visited
+            if s not in seen_states and V[s] != -1.0:
+                V[s] += alpha * (G - V[s])
+
+    # return the updated estimated value
+    return V
