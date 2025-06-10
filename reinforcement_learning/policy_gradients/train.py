1dc76e81077fc60df7b9b2baa9d8072509181ae6
LJThao <yengfong.thao@atlasschool.com>
2025-06-10 09:43:38 -0500

    shebang, docstr, func

---
+++ reinforcement_learning/policy_gradients/train.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Train Module"""
+import numpy as np
+policy_gradients = __import__('policy_gradient').policy_gradient
+
+
+def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
+    """Function that implements a full training.
+
+    env: initial environment
+    nb_episodes: number of episodes used for training
+    alpha: the learning rate
+    gamma: the discount factor
+    Return: all values of the score (sum of all rewards during one episode
+    loop)
+
+    """
