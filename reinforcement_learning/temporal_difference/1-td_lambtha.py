5c98f99d7b1778402d7031459c73c80c3473aafc
LJThao <yengfong.thao@atlasschool.com>
2025-06-09 20:55:47 -0500

    pycode

--- reinforcement_learning/temporal_difference/1-td_lambtha.py
+++ reinforcement_learning/temporal_difference/1-td_lambtha.py
@@ -9,7 +9,8 @@ def td_lambtha(env, V, policy, lambtha, episodes=5000,
 
     env is the environment instance
     V is a numpy.ndarray of shape (s,) containing the value estimate
-    policy is a function that takes in a state and returns the next action to take
+    policy is a function that takes in a state and returns the next action
+    to take
     lambtha is the eligibility trace factor
     episodes is the total number of episodes to train over
     max_steps is the maximum number of steps per episode
