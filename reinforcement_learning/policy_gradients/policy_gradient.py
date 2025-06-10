34c9a2ec0aa2711ff4555e8669f3794b5848e19e
LJThao <yengfong.thao@atlasschool.com>
2025-06-10 09:30:00 -0500

    policy func

--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -3,3 +3,15 @@
 import numpy as np
 
 
+def policy(matrix, weight):
+    """Function that computes the policy with a weight of a matrix"""
+    # raw scores
+    scores = matrix @ weight
+    # for numerical stability
+    scores -= np.max(scores, axis=1, keepdims=True)
+    # exponentiation
+    exp_scores = np.exp(scores)
+    # normalizing
+    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+
+    return probs
