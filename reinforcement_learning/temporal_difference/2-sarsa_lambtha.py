96810174427c026ecb3418c612f6d722166dd72a
LJThao <yengfong.thao@atlasschool.com>
2025-06-09 21:32:13 -0500

    pycode

--- reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -2,6 +2,7 @@
 """SARSA(λ) Module"""
 import numpy as np
 
+
 def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1,
                   gamma=0.99, epsilon=1, min_epsilon=0.1, epsilon_decay=0.05):
     """Function that performs SARSA(λ):
