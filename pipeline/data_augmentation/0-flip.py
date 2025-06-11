229d561b9fe48a7598a5c95ad236a015e92560e1
LJThao <yengfong.thao@atlasschool.com>
2025-06-10 21:22:54 -0500

    functions

---
+++ pipeline/data_augmentation/0-flip.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Flip Module"""
+import tensorflow as tf
+
+
+def flip_image(image):
+    """Function that flips an image horizontally:
+
+    image is a 3D tf.Tensor containing the image to flip
+    Returns the flipped image
+
+    """
+    return tf.image.flip_left_right(image)
---
+++ pipeline/data_augmentation/2-rotate.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Rotate Module"""
+import tensorflow as tf
+
+
+def rotate_image(image):
+    """Function that rotates an image by 90 degrees counter-clockwise:
+
+    image is a 3D tf.Tensor containing the image to rotate
+    Returns the rotated image
+
+    """
+    return tf.image.rot90(image, k=1)
