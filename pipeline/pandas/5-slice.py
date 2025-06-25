4b305d179c4b28b576d35bde87ff49b749336eaf
LJThao <yengfong.thao@atlasschool.com>
2025-06-25 18:43:14 -0500

    slice func

---
+++ pipeline/pandas/5-slice.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""To Slice Module"""
+import pandas as pd
+
+
+def slice(df):
+    """Function that takes a pd.DataFrame and:
+
+    Extracts the columns High, Low, Close, and Volume_BTC.
+    Selects every 60th row from these columns.
+    Returns: the sliced pd.DataFrame
+
+    """
