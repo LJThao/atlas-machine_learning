ff12e042f05b3d1c61bf4dd262b87782a827ea7e
LJThao <yengfong.thao@atlasschool.com>
2025-06-25 17:55:54 -0500

    numpy module

---
+++ pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""From Numpy Module"""
+import pandas as pd
+
+
+def from_numpy(array): 
+    """Function that creates a pd.DataFrame from a np.ndarray:
+
+    array is the np.ndarray from which you should create the pd.DataFrame
+    The columns of the pd.DataFrame should be labeled in alphabetical order
+    and capitalized. There will not be more than 26 columns.
+    Returns: the newly created pd.DataFrame
+
+    """
