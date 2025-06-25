a7d37ae54c6d734ca296aa0bd3024ceb0abaad96
LJThao <yengfong.thao@atlasschool.com>
2025-06-25 18:15:16 -0500

    from_dic script

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -3,3 +3,8 @@
 import pandas as pd
 
 
+# create df with labeled rows and columns
+df = pd.DataFrame({
+    'First': [0.0, 0.5, 1.0, 1.5],
+    'Second': ['one', 'two', 'three', 'four']
+}, index=['A', 'B', 'C', 'D'])
