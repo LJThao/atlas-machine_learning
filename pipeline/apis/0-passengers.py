eca76b21ad0808ee9f43b96bb21458164cc6f469
LJThao <yengfong.thao@atlasschool.com>
2025-06-25 22:24:10 -0500

    availableships func

---
+++ pipeline/apis/0-passengers.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Available Ships Module"""
+import requests
+
+
+def availableShips(passengerCount):
+    """Function to create a method that returns the list of ships that can
+    hold a given number of passengers:
+    
+    If no ship available, return an empty list.
+    
+    """
+    url = 'https://swapi-api.hbtn.io/api/starships/'
+    ships = []
+
+    while url:
+        res = requests.get(url)
+        data = res.json()
+
+        for ship in data['results']:
+            # get the # of passengers and remove commas
+            passengers = ship.get('passengers', '0').replace(',', '')
+            if passengers.isdigit() and int(passengers) >= passengerCount:
+                ships.append(ship['name'])
+
+        # move to the next
+        url = data.get('next')
+
+    return ships
