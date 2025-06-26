9f2c2d31a984590555623b01b2bf243ef14e5c9d
LJThao <yengfong.thao@atlasschool.com>
2025-06-25 19:49:53 -0500

    added docstr and update

--- pipeline/pandas/14-visualize.py
+++ pipeline/pandas/14-visualize.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-
+"""Visualize Module"""
 import matplotlib.pyplot as plt
 import pandas as pd
 from_file = __import__('2-from_file').from_file
+df = from_file('/root/atlas-machine_learning/pipeline/pandas/data/coinbase.csv', ',')
 
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
 # YOUR CODE HERE
