da813d1ec3c99df4ed428ec29c876eb1add4abe4
LJThao <yengfong.thao@atlasschool.com>
2025-07-02 16:13:29 -0500

    update

--- pipeline/databases/17-store.sql
+++ pipeline/databases/17-store.sql
@@ -1,3 +1,16 @@
 -- Creates a trigger that decreases the quantity of an item after adding a new order.
-DELETE FROM users
-WHERE email LIKE '%@user.com%';
+DROP TRIGGER IF EXISTS trg_update_inventory;
+
+DELIMITER $$
+
+CREATE TRIGGER trg_update_inventory
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END;
+$$
+
+DELIMITER ;
