f2a3080f4702fec9d383b72970fe0e7fda490078
LJThao <yengfong.thao@atlasschool.com>
2025-07-02 16:14:23 -0500

    update

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -1,17 +1,22 @@
 -- Creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
-BEGIN
-    DECLARE avg_score FLOAT;
+DROP PROCEDURE IF EXISTS UpdateUserAverage;
 
-    SELECT AVG(score) INTO avg_score
+CREATE PROCEDURE UpdateUserAverage(
+    IN target_user INT
+)
+BEGIN
+    DECLARE avg_result DECIMAL(5,2);
+
+    SELECT IFNULL(AVG(score), 0)
+    INTO avg_result
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE user_id = target_user;
 
     UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
+    SET average_score = avg_result
+    WHERE id = target_user;
 END;
 //
 
